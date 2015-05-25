68c3f6ed4e2198fbc9622f673a8216147531835f
zone117x <zone117x@gmail.com>
2015-05-25 17:50:54 -0600

    Retry http extension

---
+++ src/Jackett/HttpClientExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jackett
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<string> GetStringAsync(this HttpClient client, string uri, int retries)
+        {
+            Exception exception = null;
+            try
+            {
+                return await client.GetStringAsync(uri);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            if (retries > 0)
+                return await client.GetStringAsync(uri, --retries);
+            throw exception;
+        }
+    }
+}
