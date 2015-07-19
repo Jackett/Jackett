58911849b7d2f009c67ef9db2a4affd9a32f3435
KZ <KZ@no.mail>
2015-07-19 14:33:27 +0100

    Tray

--- src/Jackett.Tray/Program.cs
+++ src/Jackett.Tray/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Jackett.Tray
+namespace JackettTray
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace Jackett.Tray
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-           // Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
