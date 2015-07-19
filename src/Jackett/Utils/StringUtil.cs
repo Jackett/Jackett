c92caf18c57d9461a8edb7a209630eed0cf01d91
unknown <zone117x@gmail.com>
2015-07-19 11:37:08 -0600

    Fix indexer class lookup by string when name has non-alphanumeric chars

---
+++ src/Jackett/Utils/StringUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Jackett.Utils
+{
+    public static class StringUtil
+    {
+        public static string StripNonAlphaNumeric(string str)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9 -]");
+            str = rgx.Replace(str, "");
+            return str;
+        }
+
+    }
+}
