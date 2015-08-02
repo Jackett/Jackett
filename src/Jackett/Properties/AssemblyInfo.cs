b651e8a794f1a859d5eefbc0c9e2204009241a85
unknown <zone117x@gmail.com>
2015-08-02 15:55:23 -0600

    Fixed version in Inno script

--- src/Jackett/Properties/AssemblyInfo.cs
+++ src/Jackett/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
