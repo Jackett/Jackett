f7efd83e09c6d1acf4ea5da7f6b83b44098512ac
twistedpear <twistedroutes76@gmail.com>
2015-12-28 09:53:59 -0500

    Put ProxyConnection back into Startup.cs

--- src/Jackett/Startup.cs
+++ src/Jackett/Startup.cs
@@ -40,6 +40,12 @@ namespace Jackett
             set;
         }
 
+        public static string ProxyConnection
+        {
+            get;
+            set;
+        }
+
         public static bool? DoSSLFix
         {
             get;
