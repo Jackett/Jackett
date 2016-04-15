505ddc3b926044589dc266c11ec5d756c5935246
coolius <coolius@gmail.com>
2016-04-15 11:09:36 +0100

    Added AlternateLink to IPTorrents

--- src/Jackett/Models/IndexerConfig/ConfigurationDataBasicLogin.cs
+++ src/Jackett/Models/IndexerConfig/ConfigurationDataBasicLogin.cs
@@ -12,12 +12,14 @@ namespace Jackett.Models.IndexerConfig
         public StringItem Username { get; private set; }
         public StringItem Password { get; private set; }
         public DisplayItem Instructions { get; private set; }
+        public StringItem AlternateLink { get; set; }
 
         public ConfigurationDataBasicLogin(string instructionMessageOptional = null)
         {
             Username = new StringItem { Name = "Username" };
             Password = new StringItem { Name = "Password" };
             Instructions = new DisplayItem(instructionMessageOptional) { Name = "" };
+            AlternateLink = new StringItem { Name = "Alternate Link" };
         }
 
 
