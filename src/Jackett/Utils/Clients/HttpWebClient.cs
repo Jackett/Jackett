e364b321e14038ec291a9f76bb90b07b66e7c81a
JigSawFr <jiigsaaw@protonmail.ch>
2016-05-14 00:35:04 +0200

    Workaround to use TLS 1.2 & 1.1 on Mono < 4.3

--- src/Jackett/Utils/Clients/HttpWebClient.cs
+++ src/Jackett/Utils/Clients/HttpWebClient.cs
@@ -70,8 +70,11 @@ namespace Jackett.Utils.Clients
                 useProxy = true;
             }
 
+            // SecurityProtocolType values below not available in Mono < 4.3 
+            const int SecurityProtocolTypeTls11 = 768;
+            const int SecurityProtocolTypeTls12 = 3072;
             // Specify to use TLS 1.2 as default connection
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            ServicePointManager.SecurityProtocol |= (SecurityProtocolType)(SecurityProtocolTypeTls12 | SecurityProtocolTypeTls11);
 
             var client = new HttpClient(new HttpClientHandler
             {
