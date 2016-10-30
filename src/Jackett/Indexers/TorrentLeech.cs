0a577e64d1f1f6c08bfdefcd45e041d4b6d4d54f
kaso17 <swmm38@gmail.com>
2016-10-30 13:21:32 +0100

    Fix TorrentLeech (#599)

--- src/Jackett/Indexers/TorrentLeech.cs
+++ src/Jackett/Indexers/TorrentLeech.cs
@@ -181,7 +181,7 @@ namespace Jackett.Indexers
                     var category = qRow.Find(".category a").Attr("href").Replace("/torrents/browse/index/categories/", string.Empty);
                     release.Category = MapTrackerCatToNewznab(category);
 
-                    var grabs = qRow.Find("td:nth-child(5)").Get(0).FirstChild.ToString();
+                    var grabs = qRow.Find("td:nth-child(6)").Get(0).FirstChild.ToString();
                     release.Grabs = ParseUtil.CoerceInt(grabs);
 
                     release.DownloadVolumeFactor = 1;
