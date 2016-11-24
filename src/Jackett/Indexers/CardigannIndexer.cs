23c13969aed4fd147cac4518c0ddf47a6e41d816
kaso17 <swmm38@gmail.com>
2016-11-24 08:31:39 +0100

    Cardigann enhancements (#728)
    * Cardigann: Use GET for landing site request
    
    * Cardigann: Add support for magnet links

--- src/Jackett/Indexers/CardigannIndexer.cs
+++ src/Jackett/Indexers/CardigannIndexer.cs
@@ -333,7 +333,7 @@ namespace Jackett.Indexers
                 var pairs = new Dictionary<string, string>();
 
                 configData.CookieHeader.Value = null;
-                var landingResult = await RequestLoginAndFollowRedirect(LoginUrl, pairs, null, false, null, SiteLink, true);
+                var landingResult = await RequestStringWithCookies(LoginUrl, null, SiteLink);
 
                 var htmlParser = new HtmlParser();
                 var landingResultDocument = htmlParser.Parse(landingResult.Content);
@@ -692,7 +692,15 @@ namespace Jackett.Indexers
                                 switch (Field.Key)
                                 {
                                     case "download":
-                                        release.Link = resolvePath(value);
+                                        if (value.StartsWith("magnet:"))
+                                        {
+                                            release.MagnetUri = new Uri(value);
+                                            release.Link = release.MagnetUri;
+                                        }
+                                        else
+                                        {
+                                            release.Link = resolvePath(value);
+                                        }
                                         break;
                                     case "details":
                                         var url = resolvePath(value);
