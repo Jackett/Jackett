772df7a4c1d1fb72df9b0ea5a059ec8f9b2f132d
kaso17 <kaso17@users.noreply.github.com>
2016-12-29 15:54:27 +0100

    x264: no longer have to set PublishDate

--- src/Jackett/Indexers/x264.cs
+++ src/Jackett/Indexers/x264.cs
@@ -187,9 +187,6 @@ namespace Jackett.Indexers
                         release.DownloadVolumeFactor = 1;
                     release.UploadVolumeFactor = 1;
 
-                    // time is needed for sonarr, just set it to now
-                    release.PublishDate = DateTime.Now;
-
                     releases.Add(release);
                 }
             }
