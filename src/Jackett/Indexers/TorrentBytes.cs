8a149ebe479f89a5c055b91dcd44019660f7d202
kaso17 <kaso17@users.noreply.github.com>
2017-01-28 07:07:44 +0100

    update type

--- src/Jackett/Indexers/TorrentBytes.cs
+++ src/Jackett/Indexers/TorrentBytes.cs
@@ -43,7 +43,7 @@ namespace Jackett.Indexers
         {
             Encoding = Encoding.GetEncoding("iso-8859-1");
             Language = "en-us";
-            Type = "private";
+            Type = "semi-private";
 
             AddCategoryMapping(41, TorznabCatType.TV);
             AddCategoryMapping(33, TorznabCatType.TVSD);
