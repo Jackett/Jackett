7211dfe07d890c965ec75cc058a5ce2421b33a02
kaso17 <kaso17@users.noreply.github.com>
2017-03-13 19:08:25 +0100

    add Psytorrents tracker

--- README.md
+++ README.md
@@ -139,6 +139,7 @@ Developer note: The software implements the [Torznab](https://github.com/Sonarr/
  * PolishSource
  * Pretome
  * PrivateHD
+ * Psytorrents
  * PTFiles
  * QcTorrent
  * Redacted (PassTheHeadphones)
---
+++ src/Jackett/Indexers/Psytorrents.cs
@@ -0,0 +1,29 @@
+﻿using Jackett.Models;
+using NLog;
+using Jackett.Services;
+using Jackett.Utils.Clients;
+using Jackett.Indexers.Abstract;
+
+namespace Jackett.Indexers
+{
+    public class Psytorrents : GazelleTracker, IIndexer
+    {
+        public Psytorrents(IIndexerManagerService indexerManager, IWebClient webClient, Logger logger, IProtectionService protectionService)
+            : base(name: "Psytorrents",
+                desc: null,
+                link: "https://psytorrents.info/",
+                indexerManager: indexerManager,
+                logger: logger,
+                protectionService: protectionService,
+                webClient: webClient
+                )
+        {
+            Language = "en-us";
+            Type = "private";
+
+            AddCategoryMapping(1, TorznabCatType.Audio, "Music");
+            AddCategoryMapping(2, TorznabCatType.Movies, "Movies");
+            AddCategoryMapping(3, TorznabCatType.PC0day, "App");
+        }
+    }
+}
