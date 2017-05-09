8db93ccdfbe43f19bf73ceeccb564809cc308992
chibidev <zsolt.erhardt@gmail.com>
2017-05-09 17:45:05 +0200

    Moving things to the appropriate place

--- src/Jackett/Indexers/AggregateIndexer.cs
+++ src/Jackett/Indexers/AggregateIndexer.cs
@@ -17,30 +17,5 @@ namespace Jackett.Indexers
             : base("AggregateSearch", "http://127.0.0.1/", "This feed includes all configured trackers", i, wc, l, new Models.IndexerConfig.ConfigurationData(), ps)
         {
         }
-
-        public override async Task<IEnumerable<ReleaseInfo>> PerformQuery(TorznabQuery query)
-        {
-            var tasks = new List<Task<IEnumerable<ReleaseInfo>>>();
-            foreach (var indexer in Indexers.Where(i => i.CanHandleQuery(query)))
-                tasks.Add(indexer.PerformQuery(query));
-
-            var t = Task.WhenAll<IEnumerable<ReleaseInfo>>(tasks);
-            try
-            {
-                t.Wait();
-            }
-            catch (AggregateException exception)
-            {
-                logger.Error(exception, "Error during request from Aggregate");
-            }
-
-            IEnumerable<ReleaseInfo> result = tasks.Where(x => x.Status == TaskStatus.RanToCompletion).SelectMany(x => x.Result).OrderByDescending(r => r.PublishDate);
-            // Limiting the response size might be interesting for use-cases where there are
-            // tons of trackers configured in Jackett. For now just use the limit param if
-            // someone wants to do that.
-            if (query.Limit > 0)
-                result = result.Take(query.Limit);
-            return result;
-        }
     }
 }
--- src/Jackett/Indexers/BaseMetaIndexer.cs
+++ src/Jackett/Indexers/BaseMetaIndexer.cs
@@ -11,14 +11,12 @@ using NLog;
 
 namespace Jackett.Indexers
 {
-    public abstract class BaseMetaIndexer : BaseIndexer, IIndexer
+    public class BaseMetaIndexer : BaseIndexer, IIndexer
     {
         protected BaseMetaIndexer(string name, string link, string description, IIndexerManagerService manager, IWebClient client, Logger logger, ConfigurationData configData, IProtectionService p, TorznabCapabilities caps = null, string downloadBase = null)
             : base(name, link, description, manager, client, logger, configData, p, caps, downloadBase)
         {
         }
-
-        public abstract Task<IEnumerable<ReleaseInfo>> PerformQuery (TorznabQuery query);
 
         public Task<IndexerConfigurationStatus> ApplyConfiguration(JToken configJson)
         {
@@ -42,6 +40,28 @@ namespace Jackett.Indexers
 
             base.TorznabCaps = caps;
             base.IsConfigured = true;
+        }
+
+        public virtual async Task<IEnumerable<ReleaseInfo>> PerformQuery (TorznabQuery query)
+        {
+            var tasks = new List<Task<IEnumerable<ReleaseInfo>>> ();
+            foreach (var indexer in Indexers.Where (i => i.CanHandleQuery (query)))
+                tasks.Add (indexer.PerformQuery (query));
+
+            var t = Task.WhenAll<IEnumerable<ReleaseInfo>> (tasks);
+            try {
+                t.Wait ();
+            } catch (AggregateException exception) {
+                logger.Error (exception, "Error during request in metaindexer " + ID);
+            }
+
+            IEnumerable<ReleaseInfo> result = tasks.Where (x => x.Status == TaskStatus.RanToCompletion).SelectMany (x => x.Result).OrderByDescending (r => r.PublishDate);
+            // Limiting the response size might be interesting for use-cases where there are
+            // tons of trackers configured in Jackett. For now just use the limit param if
+            // someone wants to do that.
+            if (query.Limit > 0)
+                result = result.Take (query.Limit);
+            return result;
         }
 
         public override Uri UncleanLink (Uri link)
