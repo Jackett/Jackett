1e617ab7072291e5b0e3d3cc293ef10ec132f2d6
Guizzoni <guizzoni@gmail.com>
2017-07-10 16:19:22 -0300

    Update BJShare.cs

--- src/Jackett/Indexers/BJShare.cs
+++ src/Jackett/Indexers/BJShare.cs
@@ -282,7 +282,7 @@ namespace Jackett.Indexers
                             release.Description = release.Description.Replace(" / Free", ""); // Remove Free Tag
                             
                             release.Description = release.Description.Replace("Full HD", "1080p");
-                            release.Description = release.Description.Replace(" / HD", " / 720p");
+                            release.Description = release.Description.Replace(" / HD]", " / 720p]");
                             release.Description = release.Description.Replace("4K", "2160p");
 
                             int nBarra = release.Title.IndexOf("[");
