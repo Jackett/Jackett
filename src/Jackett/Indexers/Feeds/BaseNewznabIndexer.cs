dba63857e436fa3e643d7b5f5cdd5b5e4f471c7a
chibidev <zsolt.erhardt@gmail.com>
2017-08-08 09:27:21 +0200

    Improve results and fix download link on AnimeTosho

--- src/Jackett/Indexers/Feeds/BaseNewznabIndexer.cs
+++ src/Jackett/Indexers/Feeds/BaseNewznabIndexer.cs
@@ -21,29 +21,31 @@ namespace Jackett.Indexers
         {
             var doc = XDocument.Parse(feedContent);
 
-            var results = doc.Descendants("item").Select((XElement item) =>
-            {
-                var attributes = item.Descendants().Where(e => e.Name.LocalName == "attr");
-                var release = new ReleaseInfo
-                {
-                    Title = item.FirstValue("title"),
-                    Guid = item.FirstValue("guid").ToUri(),
-                    Link = item.FirstValue("link").ToUri(),
-                    Comments = item.FirstValue("comments").ToUri(),
-                    PublishDate = item.FirstValue("pubDate").ToDateTime(),
-                    Category = new List<int> { Int32.Parse(attributes.First(e => e.Attribute("name").Value == "category").Attribute("value").Value) },
-                    Size = ReadAttribute(attributes, "size").TryParse<Int64>(),
-                    Files = ReadAttribute(attributes, "files").TryParse<Int64>(),
-                    Description = item.FirstValue("description"),
-                    Seeders = ReadAttribute(attributes, "seeders").TryParse<Int32>(),
-                    Peers = ReadAttribute(attributes, "peers").TryParse<Int32>(),
-                    InfoHash = attributes.First(e => e.Attribute("name").Value == "infohash").Attribute("value").Value,
-                    MagnetUri = attributes.First(e => e.Attribute("name").Value == "magneturl").Attribute("value").Value.ToUri(),
-                };
-                return release;
-            });
+            var results = doc.Descendants("item").Select(ResultFromFeedItem);
 
             return results;
+        }
+
+        protected virtual ReleaseInfo ResultFromFeedItem(XElement item)
+        {
+            var attributes = item.Descendants().Where(e => e.Name.LocalName == "attr");
+            var release = new ReleaseInfo
+            {
+                Title = item.FirstValue("title"),
+                Guid = item.FirstValue("guid").ToUri(),
+                Link = item.FirstValue("link").ToUri(),
+                Comments = item.FirstValue("comments").ToUri(),
+                PublishDate = item.FirstValue("pubDate").ToDateTime(),
+                Category = new List<int> { Int32.Parse(attributes.First(e => e.Attribute("name").Value == "category").Attribute("value").Value) },
+                Size = ReadAttribute(attributes, "size").TryParse<Int64>(),
+                Files = ReadAttribute(attributes, "files").TryParse<Int64>(),
+                Description = item.FirstValue("description"),
+                Seeders = ReadAttribute(attributes, "seeders").TryParse<Int32>(),
+                Peers = ReadAttribute(attributes, "peers").TryParse<Int32>(),
+                InfoHash = attributes.First(e => e.Attribute("name").Value == "infohash").Attribute("value").Value,
+                MagnetUri = attributes.First(e => e.Attribute("name").Value == "magneturl").Attribute("value").Value.ToUri(),
+            };
+            return release;
         }
 
         private string ReadAttribute(IEnumerable<XElement> attributes, string attributeName)
