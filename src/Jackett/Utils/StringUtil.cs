f5ea02cccb01f65021b27f888c8cbd83c44cd312
JigSawFr <JigSawFr@users.noreply.github.com>
2017-08-13 16:25:55 +0200

    feat(utils): added sha1 hash function and refactored md5 hash function

--- src/Jackett/Utils/StringUtil.cs
+++ src/Jackett/Utils/StringUtil.cs
@@ -55,20 +55,46 @@ namespace Jackett.Utils
             return new FormUrlEncodedContent(dict).ReadAsStringAsync().Result;
         }
 
-        public static string Hash(string input)
+        /// <summary>
+        /// Convert an array of bytes to a string of hex digits
+        /// </summary>
+        /// <param name="bytes">array of bytes</param>
+        /// <returns>String of hex digits</returns>
+        public static string HexStringFromBytes(byte[] bytes)
+        {
+            var sb = new StringBuilder();
+            foreach (byte b in bytes)
+            {
+                var hex = b.ToString("x2");
+                sb.Append(hex);
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Compute hash for string encoded as UTF8
+        /// </summary>
+        /// <param name="s">String to be hashed</param>
+        /// <returns>40-character hex string</returns>
+        public static string HashSHA1(string s)
+        {
+            var sha1 = SHA1.Create();
+
+            byte[] bytes = Encoding.UTF8.GetBytes(s);
+            byte[] hashBytes = sha1.ComputeHash(bytes);
+
+            return HexStringFromBytes(hashBytes);
+        }
+
+        public static string Hash(string s)
         {
             // Use input string to calculate MD5 hash
             MD5 md5 = System.Security.Cryptography.MD5.Create();
-            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(s);
             byte[] hashBytes = md5.ComputeHash(inputBytes);
 
-            // Convert the byte array to hexadecimal string
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < hashBytes.Length; i++)
-            {
-                sb.Append(hashBytes[i].ToString("X2"));
-            }
-            return sb.ToString();
+            return HexStringFromBytes(hashBytes);
         }
 
         public static string GetExceptionDetails(this Exception exception)
