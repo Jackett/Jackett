9928815777de81fe740d0d7084921d01958d89fe
kaso17 <kaso17@users.noreply.github.com>
2017-08-21 15:02:36 +0200

    fix cookie fallback login

--- src/Jackett/Models/DTO/Config.cs
+++ src/Jackett/Models/DTO/Config.cs
@@ -5,5 +5,8 @@ namespace Jackett.Models.DTO
     {
         public string id { get; set; }
         public string value { get; set; }
+        public string cookie { get; set; } // for cookie alternative login (captcha needed + remote host)
+        public string challenge { get; set; } // for reCaptcha V1 compatibility
+        public string version { get; set; } // for reCaptcha V1 compatibility
     }
 }
