ed8cc4af4e1ed4dc5eafc8247860cbd78db42f9d
kaso17 <swmm38@gmail.com>
2017-09-05 19:01:32 +0200

    whitespaces

--- src/Jackett/Content/custom.js
+++ src/Jackett/Content/custom.js
@@ -109,7 +109,7 @@ function reloadIndexers() {
                 item.state = "error";
             else
                 item.state = "success";
-                       
+
             if (item.type == "public") {
                 item.type_label = "success";
             }
@@ -122,7 +122,7 @@ function reloadIndexers() {
             else {
                 item.type_label = "default";
             }
-                       
+
             var main_cats_list = item.caps.filter(function(c) {
                 return c.ID < 100000;
             }).map(function(c) {
