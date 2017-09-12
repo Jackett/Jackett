0ea41690a63885e43f1723d217ee64219be6a41a
garfield69 <garfield69@outlook.com>
2017-09-13 07:09:01 +1200

    cleanup for Indexers/SceneAccess.cs

--- src/Jackett.Updater/Program.cs
+++ src/Jackett.Updater/Program.cs
@@ -193,6 +193,7 @@ namespace Jackett.Updater
                 "Definitions/dragonworld.yml",
                 "Definitions/hdclub.yml",
                 "Definitions/polishtracker.yml",
+                "Indexers/SceneAccess.cs",
             };
 
             foreach (var oldFIle in oldFiles)
