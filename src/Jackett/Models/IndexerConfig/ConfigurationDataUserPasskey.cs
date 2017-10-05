d9691998f2f067a48d4741f62c3a9bd78baaae55
garfield69 <garfield69@outlook.com>
2017-10-06 08:21:21 +1300

    Update ConfigurationDataUserPasskey.cs
    add display item instructions

--- src/Jackett/Models/IndexerConfig/ConfigurationDataUserPasskey.cs
+++ src/Jackett/Models/IndexerConfig/ConfigurationDataUserPasskey.cs
@@ -1,14 +1,16 @@
-﻿namespace Jackett.Models.IndexerConfig
+namespace Jackett.Models.IndexerConfig
 {
     public class ConfigurationDataUserPasskey : ConfigurationData
     {
         public StringItem Username { get; private set; }
         public StringItem Passkey { get; private set; }
+        public DisplayItem Instructions { get; private set; }
 
         public ConfigurationDataUserPasskey(string instructionMessageOptional = null)
         {
             Username = new StringItem { Name = "Username" };
             Passkey = new StringItem { Name = "Passkey" };
+            Instructions = new DisplayItem(instructionMessageOptional) { Name = "" };
         }
     }
 }
