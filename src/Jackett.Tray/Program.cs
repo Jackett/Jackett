0f5937b38776665a35ff6dcef788461b5fa76321
kaso17 <kaso17@users.noreply.github.com>
2017-12-01 10:28:19 +0100

    JackettTray: check if it's already running
    fixes #2181

--- src/Jackett.Tray/Program.cs
+++ src/Jackett.Tray/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,9 +15,21 @@ namespace JackettTray
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            var JacketTrayProcessName = Process.GetCurrentProcess().ProcessName;
+            var runningProcesses = Process.GetProcesses();
+            var currentSessionID = Process.GetCurrentProcess().SessionId;
+            var sameAsThisSession = runningProcesses.Where(p => p.SessionId == currentSessionID);
+            var sameAsThisSessionJacketTray = sameAsThisSession.Where(p => p.ProcessName == JacketTrayProcessName);
+            if (sameAsThisSessionJacketTray.Any())
+            {
+                MessageBox.Show("JackettTray is already running");
+            }
+            else
+            { 
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Main());
+            }
         }
     }
 }
