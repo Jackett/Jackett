364a3848eb5b13e8bcdc79e32797c2be414db0e2
Simonov Andrey <simonov@proitr.ru>
2018-02-10 11:56:35 +0500

    rutracker: add strip russian letters option

---
+++ src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataRutracker.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jackett.Models.IndexerConfig.Bespoke
+{
+    class ConfigurationDataRutracker : ConfigurationDataCaptchaLogin
+    {
+        public BoolItem StripRussianLetters { get; private set; }
+
+        public ConfigurationDataRutracker()
+            : base()
+        {
+            StripRussianLetters = new BoolItem() { Name = "Strip Russian Letters", Value = true };
+        }
+    }
+}
