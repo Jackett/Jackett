66289cb3f3cc3fe1fb82a4845f9be9397f816240
kaso17 <kaso17@users.noreply.github.com>
2018-04-06 16:47:59 +0200

    fix indexer API XML support

--- src/Jackett.Common/Models/DTO/Indexer.cs
+++ src/Jackett.Common/Models/DTO/Indexer.cs
@@ -1,27 +1,44 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using Jackett.Common.Indexers;
 
 namespace Jackett.Common.Models.DTO
 {
+    [DataContract]
     public class Capability
     {
+        [DataMember]
         public string ID { get; set; }
+        [DataMember]
         public string Name { get; set; }
     }
 
+    [DataContract]
     public class Indexer
     {
+        [DataMember]
         public string id { get; private set; }
+        [DataMember]
         public string name { get; private set; }
+        [DataMember]
         public string description { get; private set; }
+        [DataMember]
         public string type { get; private set; }
+        [DataMember]
         public bool configured { get; private set; }
+        [DataMember]
         public string site_link { get; private set; }
+        [DataMember]
         public IEnumerable<string> alternativesitelinks { get; private set; }
+        [DataMember]
         public string language { get; private set; }
+        [DataMember]
         public string last_error { get; private set; }
+        [DataMember]
         public bool potatoenabled { get; private set; }
+
+        [DataMember]
         public IEnumerable<Capability> caps { get; private set; }
 
         public Indexer(IIndexer indexer)
