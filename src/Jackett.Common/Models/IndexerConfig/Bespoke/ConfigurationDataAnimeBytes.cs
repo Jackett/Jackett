92f976916f0c5ee152be19ff9046af90725fec16
kaso17 <kaso17@users.noreply.github.com>
2018-04-26 19:30:21 +0200

    AnimeBytes: update config items

--- src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataAnimeBytes.cs
+++ src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataAnimeBytes.cs
@@ -1,9 +1,9 @@
 ﻿namespace Jackett.Common.Models.IndexerConfig.Bespoke
 {
-    class ConfigurationDataAnimeBytes : ConfigurationDataCaptchaLogin
+    class ConfigurationDataAnimeBytes : ConfigurationDataPasskey
     {
         public BoolItem IncludeRaw { get; private set; }
-        public DisplayItem DateWarning { get; private set; }
+        //public DisplayItem DateWarning { get; private set; }
         public BoolItem InsertSeason { get; private set; }
         public BoolItem AddSynonyms { get; private set; }
         public BoolItem FilterSeasonEpisode { get; private set; }
@@ -12,7 +12,7 @@
             : base()
         {
             IncludeRaw = new BoolItem() { Name = "IncludeRaw", Value = false };
-            DateWarning = new DisplayItem("This tracker does not supply upload dates so they are based off year of release.") { Name = "DateWarning" };
+            //DateWarning = new DisplayItem("This tracker does not supply upload dates so they are based off year of release.") { Name = "DateWarning" };
             InsertSeason = new BoolItem() { Name = "Prefix episode number with E0 for Sonarr Compatability", Value = false };
             AddSynonyms = new BoolItem() { Name = "Add releases for each synonym title", Value = true };
             FilterSeasonEpisode = new BoolItem() { Name = "Filter results by season/episode", Value = false };
