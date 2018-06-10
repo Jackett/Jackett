53162b4bd3d60e179ef13caf84a876416acd718e
flightlevel <flightlevel@users.noreply.github.com>
2018-06-10 12:33:16 +1000

    Add back Windows Service functionality

---
+++ src/Jackett.Service.Windows/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceProcess;
+
+namespace Jackett.Service.Windows
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
