dcbbea6c3cc81e472a79e2cbd9ba6207743a2d16
flightlevel <flightlevel@users.noreply.github.com>
2018-06-16 18:32:08 +1000

    Setup logging before DI

--- src/Jackett.Server/Helper.cs
+++ src/Jackett.Server/Helper.cs
@@ -238,64 +238,13 @@ namespace Jackett.Server
 
         public static void SetupLogging(RuntimeSettings settings, ContainerBuilder builder)
         {
-            var logFileName = settings.CustomLogFileName ?? "log.txt";
-            var logLevel = settings.TracingEnabled ? NLog.LogLevel.Debug : NLog.LogLevel.Info;
-            // Add custom date time format renderer as the default is too long
-            ConfigurationItemFactory.Default.LayoutRenderers.RegisterDefinition("simpledatetime", typeof(SimpleDateTimeRenderer));
+            Logger logger = LogManager.GetCurrentClassLogger();
 
-            var logConfig = new LoggingConfiguration();
-            var logFile = new FileTarget();
-            logConfig.AddTarget("file", logFile);
-            logFile.Layout = "${longdate} ${level} ${message} ${exception:format=ToString}";
-            logFile.FileName = Path.Combine(settings.DataFolder, logFileName);
-            logFile.ArchiveFileName = "log.{#####}.txt";
-            logFile.ArchiveAboveSize = 500000;
-            logFile.MaxArchiveFiles = 5;
-            logFile.KeepFileOpen = false;
-            logFile.ArchiveNumbering = ArchiveNumberingMode.DateAndSequence;
-            var logFileRule = new LoggingRule("*", logLevel, logFile);
-            logConfig.LoggingRules.Add(logFileRule);
-
-            var logConsole = new ColoredConsoleTarget();
-            logConfig.AddTarget("console", logConsole);
-
-            logConsole.Layout = "${simpledatetime} ${level} ${message} ${exception:format=ToString}";
-            var logConsoleRule = new LoggingRule("*", logLevel, logConsole);
-            logConfig.LoggingRules.Add(logConsoleRule);
-
-            var logService = new LogCacheService();
-            logConfig.AddTarget("service", logService);
-            var serviceRule = new LoggingRule("*", logLevel, logService);
-            logConfig.LoggingRules.Add(serviceRule);
-
-            LogManager.Configuration = logConfig;
             if (builder != null)
             {
-                builder.RegisterInstance(LogManager.GetCurrentClassLogger()).SingleInstance();
+                builder.RegisterInstance(logger).SingleInstance();
             }
         }
 
-        public static void SetLogLevel(LogLevel level)
-        {
-            foreach (var rule in LogManager.Configuration.LoggingRules)
-            {
-                if (level == LogLevel.Debug)
-                {
-                    if (!rule.Levels.Contains(LogLevel.Debug))
-                    {
-                        rule.EnableLoggingForLevel(LogLevel.Debug);
-                    }
-                }
-                else
-                {
-                    if (rule.Levels.Contains(LogLevel.Debug))
-                    {
-                        rule.DisableLoggingForLevel(LogLevel.Debug);
-                    }
-                }
-            }
-
-            LogManager.ReconfigExistingLoggers();
-        }
     }
 }
