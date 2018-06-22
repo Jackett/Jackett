012f5f05cc082a7f04df6f0959c0f87ea4f0ac3b
flightlevel <flightlevel@users.noreply.github.com>
2018-06-22 22:35:58 +1000

    Update Logging

--- src/Jackett.Server/Initialisation.cs
+++ src/Jackett.Server/Initialisation.cs
@@ -1,14 +1,9 @@
-﻿using Jackett.Common;
-using Jackett.Common.Models.Config;
-using Jackett.Common.Services;
+﻿using Jackett.Common.Models.Config;
 using Jackett.Common.Services.Interfaces;
 using Jackett.Common.Utils;
 using Jackett.Server.Services;
 using NLog;
-using NLog.Config;
-using NLog.Targets;
 using System;
-using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 
@@ -16,7 +11,6 @@ namespace Jackett.Server
 {
     public static class Initialisation
     {
-        
         public static void ProcessSettings(RuntimeSettings runtimeSettings, Logger logger)
         {
             if (runtimeSettings.ClientOverride != "httpclient" && runtimeSettings.ClientOverride != "httpclient2")
@@ -179,64 +173,6 @@ namespace Jackett.Server
         private static void RunNetSh(IProcessService processService, string args)
         {
             processService.StartProcessAndLog("netsh.exe", args);
-        }
-
-        public static LoggingConfiguration SetupLogging(RuntimeSettings settings)
-        {
-            var logFileName = settings.CustomLogFileName ?? "log.txt";
-            var logLevel = settings.TracingEnabled ? NLog.LogLevel.Debug : NLog.LogLevel.Info;
-            // Add custom date time format renderer as the default is too long
-            ConfigurationItemFactory.Default.LayoutRenderers.RegisterDefinition("simpledatetime", typeof(SimpleDateTimeRenderer));
-
-            var logConfig = new LoggingConfiguration();
-            var logFile = new FileTarget();
-            logConfig.AddTarget("file", logFile);
-            logFile.Layout = "${longdate} ${level} ${message} ${exception:format=ToString}";
-            logFile.FileName = Path.Combine(settings.DataFolder, logFileName);
-            logFile.ArchiveFileName = "log.{#####}.txt";
-            logFile.ArchiveAboveSize = 500000;
-            logFile.MaxArchiveFiles = 5;
-            logFile.KeepFileOpen = false;
-            logFile.ArchiveNumbering = ArchiveNumberingMode.DateAndSequence;
-            var logFileRule = new LoggingRule("*", logLevel, logFile);
-            logConfig.LoggingRules.Add(logFileRule);
-
-            var logConsole = new ColoredConsoleTarget();
-            logConfig.AddTarget("console", logConsole);
-
-            logConsole.Layout = "${simpledatetime} ${level} ${message} ${exception:format=ToString}";
-            var logConsoleRule = new LoggingRule("*", logLevel, logConsole);
-            logConfig.LoggingRules.Add(logConsoleRule);
-
-            var logService = new LogCacheService();
-            logConfig.AddTarget("service", logService);
-            var serviceRule = new LoggingRule("*", logLevel, logService);
-            logConfig.LoggingRules.Add(serviceRule);
-
-            return logConfig;
-        }
-
-        public static void SetLogLevel(LogLevel level)
-        {
-            foreach (var rule in LogManager.Configuration.LoggingRules)
-            {
-                if (level == LogLevel.Debug)
-                {
-                    if (!rule.Levels.Contains(LogLevel.Debug))
-                    {
-                        rule.EnableLoggingForLevel(LogLevel.Debug);
-                    }
-                }
-                else
-                {
-                    if (rule.Levels.Contains(LogLevel.Debug))
-                    {
-                        rule.DisableLoggingForLevel(LogLevel.Debug);
-                    }
-                }
-            }
-
-            LogManager.ReconfigExistingLoggers();
         }
     }
 }
