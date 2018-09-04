2d5a90ae31f0adcd6e0ff81be3c171da476767fb
kaso17 <kaso17@users.noreply.github.com>
2018-09-04 19:30:48 +0200

    BaseIndexer: add cloudflare ReCaptcha detection

--- src/Jackett.Common/Indexers/BaseIndexer.cs
+++ src/Jackett.Common/Indexers/BaseIndexer.cs
@@ -575,6 +575,11 @@ namespace Jackett.Common.Indexers
             {
                 throw new Exception("Request to " + response.Request.Url + " failed (Error " + response.Status + ") - The tracker seems to be down.");
             }
+
+            if (response.Status == System.Net.HttpStatusCode.Forbidden && response.Content.Contains("<span data-translate=\"complete_sec_check\">Please complete the security check to access</span>"))
+            {
+                throw new Exception("Request to " + response.Request.Url + " failed (Error " + response.Status + ") - The page is protected by an Cloudflare reCaptcha. The page is in aggressive DDoS mitigation mode or your IP might be blacklisted (e.g. in case of shared VPN IPs). There's no easy way of making it usable with Jackett.");
+            }
         }
 
         protected async Task FollowIfRedirect(WebClientStringResult response, string referrer = null, string overrideRedirectUrl = null, string overrideCookies = null, bool accumulateCookies = false)
