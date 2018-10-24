6c823a71052a5b4498a268bbcfc30a0d1d17edaa
Robert Dziarnowski <robert.dziarnowski@lightwaverf.com>
2018-10-24 14:53:34 +0200

    - Fix for 720pier.

--- src/Jackett.Common/Indexers/720pier.cs
+++ src/Jackett.Common/Indexers/720pier.cs
@@ -193,7 +193,7 @@ namespace Jackett.Common.Indexers
                         var DetailsResultDocument = ResultParser.Parse(detailsResult.Content);
                         var qDownloadLink = DetailsResultDocument.QuerySelector("table.table2 > tbody > tr > td > a[href^=\"/download/torrent.php?id\"]");
 
-                        release.Link = new Uri(SiteLink + qDownloadLink.GetAttribute("href"));
+                        release.Link = new Uri(SiteLink + qDownloadLink.GetAttribute("href").TrimStart('/'));
 
                         release.Seeders = ParseUtil.CoerceInt(Row.QuerySelector("span.seed").TextContent);
                         release.Peers = ParseUtil.CoerceInt(Row.QuerySelector("span.leech").TextContent) + release.Seeders;
