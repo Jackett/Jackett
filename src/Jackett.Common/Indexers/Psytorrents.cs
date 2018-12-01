d61bda4f8b22c7473884a8759b5ed11e2438c63e
kaso17 <kaso17@users.noreply.github.com>
2018-12-01 12:48:54 +0100

    Psytorrents: add expired certificate

--- src/Jackett.Common/Indexers/Psytorrents.cs
+++ src/Jackett.Common/Indexers/Psytorrents.cs
@@ -3,11 +3,16 @@ using Jackett.Common.Models;
 using Jackett.Common.Services.Interfaces;
 using Jackett.Common.Utils.Clients;
 using NLog;
+using System;
 
 namespace Jackett.Common.Indexers
 {
     public class Psytorrents : GazelleTracker
     {
+        private static readonly string[] certificateHashs = new string[] {
+            "455333CC651C249E1A91DFF8EFC2A5F8044FE956", // expired
+        };
+
         public Psytorrents(IIndexerConfigurationService configService, WebClient webClient, Logger logger, IProtectionService protectionService)
             : base(name: "Psytorrents",
                 desc: "Psytorrents (PSY) is a Private Torrent Tracker for ELECTRONIC MUSIC",
@@ -22,9 +27,13 @@ namespace Jackett.Common.Indexers
             Language = "en-us";
             Type = "private";
 
+
             AddCategoryMapping(1, TorznabCatType.Audio, "Music");
             AddCategoryMapping(2, TorznabCatType.Movies, "Movies");
             AddCategoryMapping(3, TorznabCatType.PC0day, "App");
+
+            foreach (var certificateHash in certificateHashs)
+                webclient.AddTrustedCertificate(new Uri(SiteLink).Host, certificateHash);
         }
     }
 }
