b942d7b48a1df8fa592ecbedd6b5e2f3eb238f45
balazsbanyai <morpheus1333@gmail.com>
2019-02-19 12:05:51 +0100

    Initial versions for TvStore.me(#2590) - Search only work for exact match   (Search parse all series on site and if search string match with ENG or HUN name it will have result - Currently pagination is not supported - Limit is not supported - Many other bugs :)

---
+++ src/Jackett.Common/Indexers/TVstore.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using CsQuery;
+using Jackett.Common.Models;
+using Jackett.Common.Models.IndexerConfig.Bespoke;
+using Jackett.Common.Services.Interfaces;
+using Jackett.Common.Utils;
+using Jackett.Common.Utils.Clients;
+using Newtonsoft.Json.Linq;
+using NLog;
+
+namespace Jackett.Common.Indexers
+{
+    public class TVstore : BaseWebIndexer
+    {
+
+        private string LoginUrl { get { return SiteLink + "takelogin.php"; } }
+        private string loginPageUrl { get { return SiteLink + "login.php?returnto=%2F"; } }
+        private string SearchUrl { get { return SiteLink + "torrent/br_process.php"; } }
+        private List<SeriesDetail> series = new List<SeriesDetail>(); 
+
+        private new ConfigurationDataTVstore configData
+        {
+            get { return (ConfigurationDataTVstore)base.configData; }
+            set { base.configData = value; }
+        }
+
+        public TVstore(IIndexerConfigurationService configService, WebClient wc, Logger l, IProtectionService ps)
+            : base(name: "TVstore",
+                description: "A Hungarian private torrent site.",
+                link: "https://tvstore.me/",
+                caps: new TorznabCapabilities(),
+                configService: configService,
+                client: wc,
+                logger: l,
+                p: ps,
+                configData: new ConfigurationDataTVstore())
+        {
+            Encoding = Encoding.UTF8;
+            Language = "hu-hu";
+            Type = "private";
+
+            AddCategoryMapping(1, TorznabCatType.TV);
+
+        }
+
+        public override async Task<IndexerConfigurationStatus> ApplyConfiguration(JToken configJson)
+        {
+            LoadValuesFromJson(configJson);
+
+            var loginPage = await RequestStringWithCookies(loginPageUrl, string.Empty);
+            var pairs = new Dictionary<string, string> {
+                { "username", configData.Username.Value },
+                { "password", configData.Password.Value },
+                { "back", "%2F" },
+                { "logout", "1"}
+            };
+
+            var result =   await RequestLoginAndFollowRedirect(LoginUrl, pairs, loginPage.Cookies, true, referer: SiteLink);
+            await ConfigureIfOK(result.Cookies, result.Content != null && result.Content.Contains("Főoldal"), () =>
+            {
+                throw new ExceptionWithConfigData("Error while trying to login with: Username: "+ configData.Username.Value +
+                                                  " Password: "+ configData.Password.Value, configData);
+            });
+
+
+           
+
+            return IndexerConfigurationStatus.RequiresTesting;
+        }
+
+        async Task<List<ReleaseInfo>> ParseTorrents(WebClientStringResult results, TorznabQuery query, int already_founded, int limit)
+        {
+            var releases = new List<ReleaseInfo>();
+            try
+            {
+                String content = results.Content;
+                var splits = content.Split('\\');
+                int i = 0;
+
+                ReleaseInfo release = new ReleaseInfo();
+                foreach (var s in splits)
+                {
+                    switch (i)
+                    {
+                        case 4:
+                            //ID of the series
+                            //Get IMDB id form site series database
+                            SeriesDetail seriesinfo = series.Find(x => x.id.Contains(s));
+                            if (seriesinfo != null && !s.Equals(""))
+                                release.Imdb = long.Parse(seriesinfo.imdbid);
+                            goto default;
+                        case 5:
+                            //ID of the torrent
+                            Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+
+                            string fileinfoURL = "https://tvstore.me/torrent/br_process.php?func=getToggle&id=" + s + "&w=F&pg=0&now=" + unixTimestamp;
+                            string fileinfo = (await RequestStringWithCookiesAndRetry(fileinfoURL)).Content;
+                            release.Link = new Uri(SiteLink.ToString() + "torrent/download.php?id=" + s);
+                            string[] fileinf = fileinfo.Split(new string[] { "\\\\" }, StringSplitOptions.None);
+                            if (fileinf.Length>1)
+                                release.Title = fileinf[1];
+                            goto default;
+                        /*case 6:
+                            Console.WriteLine("Series season/ep =" + s); --> 9x10
+                            goto default;*/
+                        /*case 7:
+                            Console.WriteLine("Releaseinfo =" + s);  --->Releaseinfo =[HDTV - Rip - Eng - SVA]
+                            goto default;*/
+                        case 9:
+                            release.PublishDate = DateTime.Parse(s, CultureInfo.InvariantCulture);
+                            goto default;
+                        case 13:
+                            release.Files = int.Parse(s);
+                            goto default;
+                        case 14:
+                            release.Size = long.Parse(s);
+                            goto default;
+                        case 23:
+                            release.Seeders = int.Parse(s);
+                            goto default;
+                        case 24:
+                            release.Peers = (int.Parse(s) + release.Seeders);
+                            goto default;
+                        case 25:
+                            release.Grabs = int.Parse(s);
+                            goto default;
+                        case 26:
+
+                            i = 0;
+                            release.Category = new List<int> { TvCategoryParser.ParseTvShowQuality(release.Title) };
+                            // Added some basic configuration need to improve it
+                            release.MinimumRatio = 1;
+                            release.MinimumSeedTime = 172800;
+                            release.DownloadVolumeFactor = 1;
+                            release.UploadVolumeFactor = 1;
+
+                            if ((already_founded + releases.Count) < limit)
+                                releases.Add(release);
+                            release = new ReleaseInfo();
+                            break;
+                        default:
+                            i++;
+                            break;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                OnParseError(results.Content, ex);
+            }
+
+            return releases;
+        }
+        /* Search is possible only based by Series ID. 
+         * All known series ID is on main page, with their attributes. (ID, EngName, HunName, imdbid)*/
+        protected async Task<Boolean> GetSeriesInfo()
+        {
+
+            var result = (await RequestStringWithCookiesAndRetry("https://tvstore.me/torrent/browse.php")).Content;
+
+            CQ dom = result;
+            var scripts = dom["script"];
+
+            foreach (var script in scripts)
+            {
+                if (script.TextContent.Contains("catsh=Array"))
+                {
+                    string[] seriesknowbysite = Regex.Split(script.TextContent, "catl");
+                    for (int i = 1; i < seriesknowbysite.Length; i++)
+                    {
+                        try
+                        {
+                            var id = seriesknowbysite[i];
+                            string[] serieselement = id.Split(';');
+                            SeriesDetail sd = new SeriesDetail();
+                            sd.HunName = serieselement[1].Split('=')[1].Trim('\'').ToLower();
+                            sd.EngName = serieselement[2].Split('=')[1].Trim('\'').ToLower();
+                            sd.id = serieselement[0].Split('=')[1].Trim('\'');
+                            sd.imdbid = serieselement[7].Split('=')[1].Trim('\'');
+                            series.Add(sd);
+                        }
+                        catch (IndexOutOfRangeException e)
+                        { }
+                       
+                    }
+                }
+            }
+            return true;
+        }
+
+        protected override async Task<IEnumerable<ReleaseInfo>> PerformQuery(TorznabQuery query)
+        {
+            var releases = new List<ReleaseInfo>();
+
+            /* If series from sites are indexed than we dont need it.*/
+            if (series == null || series.IsEmpty())
+            {
+                await GetSeriesInfo();
+            }
+            if (query.SearchTerm == null)
+                query.SearchTerm = "";
+
+            /*todo if not exact name is comming in SearchTerm will wont work.     
+            /* Search for series based on English name in the list parsed before */
+            SeriesDetail seriesinfo = series.Find(x => x.EngName.Contains(query.SearchTerm.ToLower()));
+            if (seriesinfo == null)
+            {
+                /* Search for series based on Hungarian name if English has no match*/
+                seriesinfo = series.Find(x => x.HunName.Contains(query.SearchTerm.ToLower()));
+            }
+            if (seriesinfo == null)
+                return releases;
+
+            Console.WriteLine("AAAA Season " + seriesinfo.id);
+
+            Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+
+            /*var pairs = new List<KeyValuePair<string, string>>();
+            pairs.Add(new KeyValuePair<string, string>("g", seriesinfo.id));
+            pairs.Add(new KeyValuePair<string, string>("s", query.Season.ToString()));
+            pairs.Add(new KeyValuePair<string, string>("e", query.Episode));
+            pairs.Add(new KeyValuePair<string, string>("now", unixTimestamp.ToString()));
+            var results = await PostDataWithCookiesAndRetry(SearchUrl, pairs);
+            */
+
+            WebClientStringResult results;
+
+            /* Create the search URL. 
+             * It will be better to use some func from Baseindexer but they wont work
+               Links looks the following https://tvstore.me/torrent/br_process.php?s=1&e=2&g=33&now=1550570305277
+             */
+            string exactSearchURL = SearchUrl;
+            if (!query.SearchTerm.Equals(""))
+            {
+                exactSearchURL += "?g=" + seriesinfo.id;
+                if (query.Season != 0)
+                    exactSearchURL += "&s=" + query.Season.ToString();
+                if (query.Episode != null && !query.Episode.Equals(""))
+                    exactSearchURL += "&e=" + query.Episode;
+                exactSearchURL += "&now=" + unixTimestamp.ToString();
+            }
+
+            results = await RequestStringWithCookiesAndRetry(exactSearchURL);
+
+            var limit = query.Limit;
+            if (limit == 0)
+                limit = 100;
+
+            releases = await ParseTorrents(results, query, releases.Count, limit);
+              
+            return releases;
+        }
+    }
+    public class SeriesDetail
+    {
+        public string id;
+        public string HunName;
+        public string EngName;
+        public string imdbid;
+
+    }
+
+}
