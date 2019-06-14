d8049ed743e66c99540557a4242ee65d626d6fea
Krypt1 <mskrypt1@gmail.com>
2019-06-15 01:23:31 +0300

    Add AniDUB tracker (#5399)

---
+++ src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataAniDub.cs
@@ -0,0 +1,16 @@
+﻿namespace Jackett.Common.Models.IndexerConfig.Bespoke
+{
+    internal class ConfigurationDataAniDub : ConfigurationDataBasicLogin
+    {
+        public BoolItem StripRussianTitle { get; private set; }
+
+        public ConfigurationDataAniDub() : base()
+        {
+            StripRussianTitle = new BoolItem
+            {
+                Name = "Strip Russian Title",
+                Value = true
+            };
+        }
+    }
+}
