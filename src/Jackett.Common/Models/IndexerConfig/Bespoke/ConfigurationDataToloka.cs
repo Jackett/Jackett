05e6dd8d34586d4977ceb828724e558ee3f25fdd
Kyrylo Mikos <kiril.mik.os@gmail.com>
2019-06-30 15:23:19 +0300

    Add Toloka.to Semi-Private Tracker.

---
+++ src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataToloka.cs
@@ -0,0 +1,13 @@
+﻿namespace Jackett.Common.Models.IndexerConfig.Bespoke
+{
+    class ConfigurationDataToloka : ConfigurationDataBasicLogin
+    {
+        public BoolItem StripCyrillicLetters { get; private set; }
+
+        public ConfigurationDataToloka()
+            : base()
+        {
+            StripCyrillicLetters = new BoolItem() { Name = "Strip Cyrillic Letters", Value = true };
+        }
+    }
+}
