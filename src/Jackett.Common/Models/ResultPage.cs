171a70693bbf8df15d21ae3ce2abce6f7ff115f4
Garfield69 <garfield69@outlook.com>
2019-10-08 10:51:24 +1300

    Revert "resultpage: support 8 digit imdbid"
    This reverts commit 2e102ee456d7a24e21be3bdc726d7f12ddf714a5.

--- src/Jackett.Common/Models/ResultPage.cs
+++ src/Jackett.Common/Models/ResultPage.cs
@@ -82,7 +82,7 @@ namespace Jackett.Common.Models
                             getTorznabElement("magneturl", r.MagnetUri),
                             getTorznabElement("rageid", r.RageID),
                             getTorznabElement("thetvdb", r.TVDBId),
-                            getTorznabElement("imdb", r.Imdb == null ? null : ((long)r.Imdb).ToString("D8")),
+                            getTorznabElement("imdb", r.Imdb == null ? null : ((long)r.Imdb).ToString("D7")),
                             getTorznabElement("seeders", r.Seeders),
                             getTorznabElement("peers", r.Peers),
                             getTorznabElement("infohash", r.InfoHash),
