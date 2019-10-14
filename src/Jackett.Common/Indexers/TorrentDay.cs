42b6707aa18ff70d471bc3ea545233c5810e536a
eriuhdkjnwwk <beatrice_97@hotmail.com>
2019-10-13 22:04:59 -0700

    add AudioMP3 / 3010 for headphones search support

--- src/Jackett.Common/Indexers/TorrentDay.cs
+++ src/Jackett.Common/Indexers/TorrentDay.cs
@@ -90,6 +90,7 @@ namespace Jackett.Common.Indexers
             AddCategoryMapping(1, TorznabCatType.MoviesSD, "Movies/XviD");
 
             AddCategoryMapping(17, TorznabCatType.Audio, "Music/Audio");
+            AddCategoryMapping(17, TorznabCatType.AudioMP3, "Music/Audio");
             AddCategoryMapping(23, TorznabCatType.AudioForeign, "Music/Non-English");
             AddCategoryMapping(41, TorznabCatType.Audio, "Music/Packs");
             AddCategoryMapping(16, TorznabCatType.AudioVideo, "Music/Video");
