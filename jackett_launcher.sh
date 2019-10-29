4d20f2f4e4f1c18478315e0b8d3f4e065eb28579
ngosang <ngosang@hotmail.es>
2019-10-29 23:51:21 +0100

    requested changes

--- jackett_launcher.sh
+++ jackett_launcher.sh
@@ -8,8 +8,11 @@ JACKETT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Launch Jackett
 ${JACKETT_DIR}/jackett
 
+# Get user running the service
+JACKETT_USER=$(whoami)
+
 # Wait until the updater ends
-while pgrep JackettUpdater > /dev/null ; do
+while pgrep -u ${JACKETT_USER} JackettUpdater > /dev/null ; do
      sleep 1
 done
 
