3eaded05646394306ac3e3e3b048b2b0a675f03f
ERR1R <nielsd@quicknet.nl>
2019-11-08 09:18:03 +0100

    Added/updated Categories

--- src/Jackett.Common/Indexers/digitalcore.cs
+++ src/Jackett.Common/Indexers/digitalcore.cs
@@ -44,15 +44,16 @@ namespace Jackett.Common.Indexers
             TorznabCaps.SupportsImdbMovieSearch = true;
 
             AddCategoryMapping(1, TorznabCatType.MoviesDVD, "Movies/DVDR");
-            AddCategoryMapping(2, TorznabCatType.MoviesSD, "Movies/XviD");
+            AddCategoryMapping(2, TorznabCatType.MoviesSD, "Movies/SD");
             AddCategoryMapping(3, TorznabCatType.MoviesBluRay, "Movies/BluRay");
             AddCategoryMapping(4, TorznabCatType.MoviesUHD, "Movies/4K");
             AddCategoryMapping(5, TorznabCatType.MoviesHD, "Movies/720p");
             AddCategoryMapping(6, TorznabCatType.MoviesHD, "Movies/1080p");
+            AddCategoryMapping(7, TorznabCatType.MoviesHD, "Movies/PACKS");
 
             AddCategoryMapping(8, TorznabCatType.TVHD, "Tv/720p");
             AddCategoryMapping(9, TorznabCatType.TVHD, "Tv/1080p");
-            AddCategoryMapping(10, TorznabCatType.TVSD, "Tv/XVID");
+            AddCategoryMapping(10, TorznabCatType.TVSD, "Tv/SD");
             AddCategoryMapping(11, TorznabCatType.TVSD, "Tv/DVDR");
             AddCategoryMapping(12, TorznabCatType.TVHD, "Tv/PACKS");
             AddCategoryMapping(13, TorznabCatType.TVUHD, "Tv/4K");
@@ -60,12 +61,18 @@ namespace Jackett.Common.Indexers
 
             AddCategoryMapping(17, TorznabCatType.Other, "Unknown");
             AddCategoryMapping(18, TorznabCatType.PC0day, "Apps/0day");
-            AddCategoryMapping(19, TorznabCatType.PCGames, "Games/PC");
             AddCategoryMapping(20, TorznabCatType.PCISO, "Apps/PC");
+            AddCategoryMapping(21, TorznabCatType.PCMac, "Apps/Mac");
 
             AddCategoryMapping(22, TorznabCatType.AudioMP3, "Music/MP3");
             AddCategoryMapping(23, TorznabCatType.AudioLossless, "Music/FLAC");
             AddCategoryMapping(24, TorznabCatType.Audio, "Music/MTV");
+
+            AddCategoryMapping(25, TorznabCatType.PCGames, "Games/PC");
+            AddCategoryMapping(26, TorznabCatType.Console, "Games/NSW");
+            AddCategoryMapping(27, TorznabCatType.PCMac, "Games/Mac");
+            
+            AddCategoryMapping(28, TorznabCatType.Books, "Ebooks");
 
             AddCategoryMapping(30, TorznabCatType.XXX, "XXX/SD");
             AddCategoryMapping(31, TorznabCatType.XXX, "XXX/HD");
