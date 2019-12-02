3c031b3e87a65d0671e314482e6b5b26f440e1ff
lpearl <45906593+lpearl@users.noreply.github.com>
2019-12-01 23:48:13 -0500

    Pornolab
    Adding Pornolab support

---
+++ src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataPornolab.cs
@@ -0,0 +1,13 @@
+﻿namespace Jackett.Common.Models.IndexerConfig.Bespoke
+{
+    class ConfigurationDataPornolab : ConfigurationDataCaptchaLogin
+    {
+        public BoolItem StripRussianLetters { get; private set; }
+
+        public ConfigurationDataPornolab()
+            : base()
+        {
+            StripRussianLetters = new BoolItem() { Name = "Strip Russian Letters", Value = true };
+        }
+    }
+}
