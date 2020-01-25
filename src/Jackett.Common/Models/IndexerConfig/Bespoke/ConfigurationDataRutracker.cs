f8cbc8848b851dd4849b5269820a62ef2766eb58
Garfield69 <garfield69@outlook.com>
2020-01-26 10:18:25 +1300

    rutracker: add captcha info to config. https://github.com/Jackett/Jackett/pull/6994#issuecomment-577585897

--- src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataRutracker.cs
+++ src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataRutracker.cs
@@ -3,11 +3,13 @@
     class ConfigurationDataRutracker : ConfigurationDataCaptchaLogin
     {
         public BoolItem StripRussianLetters { get; private set; }
+        public DisplayItem CaptchaWarning { get; private set; }
 
         public ConfigurationDataRutracker()
             : base()
         {
             StripRussianLetters = new BoolItem() { Name = "Strip Russian Letters", Value = true };
+            CaptchaWarning = new DisplayItem("<b>About Captcha:</b> If the Captcha Image is missing then leave the Captcha Text empty.") { Name = "Captcha Info" };
         }
     }
 }
