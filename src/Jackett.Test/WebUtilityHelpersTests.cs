4d16997241788965c23a3b0b95a97be7688d7774
junglebus <22320807+junglebus@users.noreply.github.com>
2020-01-26 21:46:12 +1100

    Allow unit tests to run in non-Windows environments

--- src/Jackett.Test/WebUtilityHelpersTests.cs
+++ src/Jackett.Test/WebUtilityHelpersTests.cs
@@ -15,12 +15,7 @@ namespace Jackett.Test
         public WebUtilityHelpersTests()
         {
             //https://docs.microsoft.com/en-us/dotnet/api/system.text.codepagesencodingprovider?view=netcore-2.0
-#if !NET461
-            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
-            {
-                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            }
-#endif
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
             _codePagesToTest = new Encoding[]{
                 Encoding.UTF8,
