b3e837699ddab10b8f91adbf8e1c48fe32adccaa
junglebus <22320807+junglebus@users.noreply.github.com>
2020-02-15 10:40:06 +1100

    Use ComImport to create shortcut
    Using the url shortcut wasn't reliable

--- src/Jackett.Tray/Main.cs
+++ src/Jackett.Tray/Main.cs
@@ -3,6 +3,9 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Jackett.Common.Models.Config;
@@ -145,7 +148,7 @@ namespace Jackett.Tray
         {
             get
             {
-                return File.Exists(ShortcutPath) || File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Startup), "Jackett.lnk"));
+                return File.Exists(ShortcutPath);
             }
             set
             {
@@ -164,24 +167,18 @@ namespace Jackett.Tray
         {
             get
             {
-                return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Startup), "Jackett.url");
+                return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Startup), "Jackett.lnk");
             }
         }
 
         private void CreateShortcut()
         {
-            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
-            {
-                using (var writer = new StreamWriter(ShortcutPath))
-                {
-                    var appPath = Process.GetCurrentProcess().MainModule.FileName;
-                    writer.WriteLine("[InternetShortcut]");
-                    writer.WriteLine("URL=file:///" + appPath);
-                    writer.WriteLine("IconIndex=0");
-                    var icon = appPath.Replace('\\', '/');
-                    writer.WriteLine("IconFile=" + icon);
-                }
-            }
+            var shellLink = (IShellLink)new ShellLink();
+            shellLink.SetDescription("Jackett");
+            shellLink.SetPath(Process.GetCurrentProcess().MainModule.FileName);
+            shellLink.SetWorkingDirectory(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName));
+            var persistFile = (IPersistFile)shellLink;
+            persistFile.Save(ShortcutPath, false);
         }
 
         private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
@@ -317,6 +314,38 @@ namespace Jackett.Tray
                 logger.Info("Tray icon not responsible for process exit");
                 CloseTrayApplication();
             }
+        }
+
+        //Used for creating the Windows startup shortcut
+        [ComImport]
+        [Guid("00021401-0000-0000-C000-000000000046")]
+        internal class ShellLink
+        {
+        }
+
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("000214F9-0000-0000-C000-000000000046")]
+        internal interface IShellLink
+        {
+            void GetPath([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszFile, int cchMaxPath, out IntPtr pfd, int fFlags);
+            void GetIDList(out IntPtr ppidl);
+            void SetIDList(IntPtr pidl);
+            void GetDescription([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszName, int cchMaxName);
+            void SetDescription([MarshalAs(UnmanagedType.LPWStr)] string pszName);
+            void GetWorkingDirectory([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszDir, int cchMaxPath);
+            void SetWorkingDirectory([MarshalAs(UnmanagedType.LPWStr)] string pszDir);
+            void GetArguments([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszArgs, int cchMaxPath);
+            void SetArguments([MarshalAs(UnmanagedType.LPWStr)] string pszArgs);
+            void GetHotkey(out short pwHotkey);
+            void SetHotkey(short wHotkey);
+            void GetShowCmd(out int piShowCmd);
+            void SetShowCmd(int iShowCmd);
+            void GetIconLocation([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszIconPath, int cchIconPath, out int piIcon);
+            void SetIconLocation([MarshalAs(UnmanagedType.LPWStr)] string pszIconPath, int iIcon);
+            void SetRelativePath([MarshalAs(UnmanagedType.LPWStr)] string pszPathRel, int dwReserved);
+            void Resolve(IntPtr hwnd, int fFlags);
+            void SetPath([MarshalAs(UnmanagedType.LPWStr)] string pszFile);
         }
     }
 }
