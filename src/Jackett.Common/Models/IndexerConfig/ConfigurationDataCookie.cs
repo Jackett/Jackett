ae6ca07bdca28fa3f630f800c338a3c1e961e831
ngosang <ngosang@hotmail.es>
2020-02-22 14:21:36 +0100

    core: improve cookie configuration help

--- src/Jackett.Common/Models/IndexerConfig/ConfigurationDataCookie.cs
+++ src/Jackett.Common/Models/IndexerConfig/ConfigurationDataCookie.cs
@@ -4,21 +4,16 @@ namespace Jackett.Common.Models.IndexerConfig
     public class ConfigurationDataCookie : ConfigurationData
     {
         public StringItem Cookie { get; private set; }
-        public DisplayItem CookieHint { get; private set; }
-        public DisplayItem CookieExample { get; private set; }
+        public DisplayItem CookieInstructions { get; private set; }
 
         public ConfigurationDataCookie()
         {
             Cookie = new StringItem { Name = "Cookie" };
-            CookieHint = new DisplayItem(
-            "<ol><li>Login to this tracker in your browser <li>Open the developer console, go the network tab <li>Find 'cookie' in the request headers <li>Copy & paste it to here</ol>")
+            CookieInstructions = new DisplayItem(
+            "Please enter the cookie for the site manually. <a href=\"https://github.com/Jackett/Jackett/wiki/Finding-cookies\" target=\"_blank\">See here</a> on how get the cookies." +
+            "<br>Example cookie header (usually longer than this):<br><code>PHPSESSID=8rk27odm; ipsconnect_63ad9c=1; more_stuff=etc;</code>")
             {
-                Name = "CookieHint"
-            };
-            CookieExample = new DisplayItem(
-            "Example cookie header (usually longer than this):<br><code>PHPSESSID=8rk27odm; ipsconnect_63ad9c=1; more_stuff=etc;</code>")
-            {
-                Name = "CookieExample"
+                Name = "Cookie Instructions"
             };
         }
     }
