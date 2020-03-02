Jackett/Jackett

commit d22ef13fb21482a509ce443284bd738b56471924

Convert TorrentBytes to AngleSharp (#7388)

src/Jackett.Common/Indexers/TorrentBytes.cs
@@ -4,7 +4,7 @@ using System.Collections.Specialized;
 using System.Globalization;
 using System.Text;
 using System.Threading.Tasks;
-using CsQuery;
+using AngleSharp.Html.Parser;
 using Jackett.Common.Models;
 using Jackett.Common.Models.IndexerConfig;
 using Jackett.Common.Services.Interfaces;
@@ -92,11 +92,10 @@ namespace Jackett.Common.Indexers
             var result = await RequestLoginAndFollowRedirect(LoginUrl, pairs, loginPage.Cookies, true, SiteLink, SiteLink);
             await ConfigureIfOK(result.Cookies, result.Content != null && result.Content.Contains("my.php"), () =>
             {
-                CQ dom = result.Content;
-                var messageEl = dom["td.embedded"].First();
-                var errorMessage = messageEl.Text();
-                if (string.IsNullOrWhiteSpace(errorMessage))
-                    errorMessage = result.Content;
+                var parser = new HtmlParser();
+                var dom = parser.ParseDocument(result.Content);
+                var messageEl = dom.QuerySelector("td.embedded");
+                var errorMessage = messageEl != null ? messageEl.TextContent : result.Content;
                 throw new ExceptionWithConfigData(errorMessage, configData);
             });
             return IndexerConfigurationStatus.RequiresTesting;
@@ -143,66 +142,58 @@ namespace Jackett.Common.Indexers
                 response = await RequestStringWithCookiesAndRetry(searchUrl, null, BrowseUrl);
             }
 
-            var results = response.Content;
             try
             {
-                CQ dom = results;
-
-                var rows = dom["table > tbody:has(tr > td.colhead) > tr:not(:has(td.colhead))"];
+                var parser = new HtmlParser();
+                var dom = parser.ParseDocument(response.Content);
+                var rows = dom.QuerySelectorAll("table > tbody:has(tr > td.colhead) > tr:not(:has(td.colhead))");
                 foreach (var row in rows)
                 {
                     var release = new ReleaseInfo();
 
-                    var link = row.Cq().Find("td:eq(1) a:eq(1)").First();
-                    release.Guid = new Uri(SiteLink + link.Attr("href"));
+                    var link = row.QuerySelector("td:nth-of-type(2) a:nth-of-type(2)");
+                    release.Guid = new Uri(SiteLink + link.GetAttribute("href"));
                     release.Comments = release.Guid;
-                    release.Title = link.Attr("title");
+                    release.Title = link.GetAttribute("title");
 
                     // There isn't a title attribute if the release name isn't truncated.
                     if (string.IsNullOrWhiteSpace(release.Title))
-                    {
-                        release.Title = link.Get(0).FirstChild.ToString().Trim();
-                    }
+                        release.Title = link.FirstChild.ToString().Trim();
 
                     release.Description = release.Title;
 
                     // If we search an get no results, we still get a table just with no info.
                     if (string.IsNullOrWhiteSpace(release.Title))
-                    {
                         break;
-                    }
 
                     // Check if the release has been assigned a category
-                    if (row.Cq().Find("td:eq(0) a").Length > 0)
+                    var qCat = row.QuerySelector("td:nth-of-type(1) a");
+                    if (qCat != null)
                     {
-                        var cat = row.Cq().Find("td:eq(0) a").First().Attr("href").Substring(15);
+                        var cat = qCat.GetAttribute("href").Substring(15);
                         release.Category = MapTrackerCatToNewznab(cat);
                     }
 
-                    var qLink = row.Cq().Find("td:eq(1) a").First();
-                    release.Link = new Uri(SiteLink + qLink.Attr("href"));
+                    var qLink = row.QuerySelector("td:nth-of-type(2) a");
+                    release.Link = new Uri(SiteLink + qLink.GetAttribute("href"));
 
-                    var added = row.Cq().Find("td:eq(4)").First().Text().Trim();
+                    var added = row.QuerySelector("td:nth-of-type(5)").TextContent.Trim();
                     release.PublishDate = DateTime.ParseExact(added, "yyyy-MM-ddHH:mm:ss", CultureInfo.InvariantCulture);
 
-                    var sizeStr = row.Cq().Find("td:eq(6)").First().Text().Trim();
+                    var sizeStr = row.QuerySelector("td:nth-of-type(7)").TextContent.Trim();
                     release.Size = ReleaseInfo.GetBytes(sizeStr);
 
-                    release.Seeders = ParseUtil.CoerceInt(row.Cq().Find("td:eq(8)").First().Text().Trim());
-                    release.Peers = ParseUtil.CoerceInt(row.Cq().Find("td:eq(9)").First().Text().Trim()) + release.Seeders;
+                    release.Seeders = ParseUtil.CoerceInt(row.QuerySelector("td:nth-of-type(9)").TextContent.Trim());
+                    release.Peers = ParseUtil.CoerceInt(row.QuerySelector("td:nth-of-type(10)").TextContent.Trim()) + release.Seeders;
 
-                    var files = row.Cq().Find("td:nth-child(3)").Text();
+                    var files = row.QuerySelector("td:nth-child(3)").TextContent;
                     release.Files = ParseUtil.CoerceInt(files);
 
-                    var grabs = row.Cq().Find("td:nth-child(8)").Text();
+                    var grabs = row.QuerySelector("td:nth-child(8)").TextContent;
                     if (grabs != "----")
                         release.Grabs = ParseUtil.CoerceInt(grabs);
 
-                    if (row.Cq().Find("font[color=\"green\"]:contains(F):contains(L)").Length >= 1)
-                        release.DownloadVolumeFactor = 0;
-                    else
-                        release.DownloadVolumeFactor = 1;
-
+                    release.DownloadVolumeFactor = row.QuerySelector("font[color=\"green\"]:contains(F):contains(L)") != null ? 0 : 1;
                     release.UploadVolumeFactor = 1;
 
                     releases.Add(release);
@@ -210,7 +201,7 @@ namespace Jackett.Common.Indexers
             }
             catch (Exception ex)
             {
-                OnParseError(results, ex);
+                OnParseError(response.Content, ex);
             }
         }
     }

commit a9d3ae0107acfa20f018d7d3b9be0ba2c1c73952

torrentbytes: fix possible parse error caused by #7388

src/Jackett.Common/Indexers/TorrentBytes.cs
@@ -193,7 +193,7 @@ namespace Jackett.Common.Indexers
                     if (grabs != "----")
                         release.Grabs = ParseUtil.CoerceInt(grabs);
 
-                    release.DownloadVolumeFactor = row.QuerySelector("font[color=\"green\"]:contains(F):contains(L)") != null ? 0 : 1;
+                    release.DownloadVolumeFactor = row.QuerySelector("font[color=\"green\"]:contains(\"F\"):contains(\"L\")") != null ? 0 : 1;
                     release.UploadVolumeFactor = 1;
 
                     releases.Add(release);