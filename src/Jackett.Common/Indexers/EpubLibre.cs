b529d54ab5ce1bcd01fe06efdbd15c226121ca78
Cory <cadatoiva@gmail.com>
2020-03-14 17:13:30 -0500

    core: rename WebClientStringResult.Content to WebClientStringResult.ContentString (#7652)

--- src/Jackett.Common/Indexers/EpubLibre.cs
+++ src/Jackett.Common/Indexers/EpubLibre.cs
@@ -88,7 +88,7 @@ namespace Jackett.Common.Indexers
 
                 try
                 {
-                    var json = JsonConvert.DeserializeObject<dynamic>(result.Content);
+                    var json = JsonConvert.DeserializeObject<dynamic>(result.ContentString);
                     var parser = new HtmlParser();
                     var doc = parser.ParseDocument((string)json["contenido"]);
 
@@ -140,7 +140,7 @@ namespace Jackett.Common.Indexers
                 }
                 catch (Exception ex)
                 {
-                    OnParseError(result.Content, ex);
+                    OnParseError(result.ContentString, ex);
                 }
             }
 
@@ -154,13 +154,13 @@ namespace Jackett.Common.Indexers
                 throw new Exception("El servidor se encuentra sobrecargado en estos momentos. / The server is currently overloaded.");
             try {
                 var parser = new HtmlParser();
-                var doc = parser.ParseDocument(result.Content);
+                var doc = parser.ParseDocument(result.ContentString);
                 var magnetLink = doc.QuerySelector("a[id=en_desc]").GetAttribute("href");
                 return Encoding.UTF8.GetBytes(magnetLink);
             }
             catch (Exception ex)
             {
-                OnParseError(result.Content, ex);
+                OnParseError(result.ContentString, ex);
             }
             return null;
         }
