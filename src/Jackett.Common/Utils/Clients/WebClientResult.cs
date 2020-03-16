8cf726e73cf314ba348ca6dd11503939711a0448
Cory <cadatoiva@gmail.com>
2020-03-16 15:17:46 -0500

    core: add implicit string result conversion (#7690)

--- src/Jackett.Common/Utils/Clients/WebClientResult.cs
+++ src/Jackett.Common/Utils/Clients/WebClientResult.cs
@@ -3,5 +3,17 @@ namespace Jackett.Common.Utils.Clients
     public class WebClientStringResult : BaseWebResult
     {
         public string ContentString { get; set; }
+
+        public static implicit operator WebClientStringResult(WebClientByteResult br) => new WebClientStringResult()
+        {
+            ContentString = br.Encoding.GetString(br.ContentBytes),
+            Cookies = br.Cookies,
+            Encoding = br.Encoding,
+            Headers = br.Headers,
+            RedirectingTo = br.RedirectingTo,
+            Request = br.Request,
+            Status = br.Status
+        };
+
     }
 }
