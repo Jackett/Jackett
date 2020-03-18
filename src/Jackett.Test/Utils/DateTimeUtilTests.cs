3fc894ba540062ee87b1b4dfd66e2af6e22d77c0
cadatoiva <cadatoiva@gmail.com>
2020-03-18 01:56:40 -0500

    Add unit tests for date parsing

---
+++ src/Jackett.Test/Utils/DateTimeUtilTests.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Jackett.Common.Utils.Tests
+{
+    [TestFixture]
+    public class DateTimeUtilTests
+    {
+        [Test]
+        public void FromUnknownTest()
+        {
+            var today = DateTime.UtcNow.Date;
+            var yesterday = today.AddDays(-1);
+            var tomorrow = today.AddDays(1);
+            var testCases = new Dictionary<string, DateTime>
+            {
+                {"today, at 1:00 PM", today.AddHours(13)},
+                {"Today", today},
+                {"Today at 20:19:54", today.AddHours(20).AddMinutes(19).AddSeconds(54)},
+                {"Today 22:29", today.AddHours(22).AddMinutes(29)},
+                {"Yesterday\n 19:54:54", yesterday.AddHours(19).AddMinutes(54).AddSeconds(54)},
+                {"Yesterday\n 11:55 PM", yesterday.AddHours(23).AddMinutes(55)}
+            };
+
+            foreach (var testCase in testCases)
+                Assert.AreEqual( testCase.Value, DateTimeUtil.FromUnknown(testCase.Key));
+        }
+    }
+}
