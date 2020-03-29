4176b0836ff151bfaaa519c479a00b6ce04956c2
cadatoiva <cadatoiva@yahoo.com>
2020-03-29 02:06:22 -0500

    Switch to const string in unit test

--- src/Jackett.Test/Utils/StringUtilTests.cs
+++ src/Jackett.Test/Utils/StringUtilTests.cs
@@ -16,9 +16,8 @@ namespace Jackett.Common.Utils.Tests
                 { "makin", "thisup" },
                 { "st", "duplicate" }
             };
-            var combinedTemplate = "q=test{0}st=troy%2Cduplicate{0}makin=thisup";
-            var splitTemplate = "q=test{0}st=troy{0}st=duplicate{0}makin=thisup";
-
+            const string combinedTemplate = "q=test{0}st=troy%2Cduplicate{0}makin=thisup";
+            const string splitTemplate = "q=test{0}st=troy{0}st=duplicate{0}makin=thisup";
             Assert.AreEqual(string.Format(combinedTemplate, "&"), testCase.GetQueryString());
             Assert.AreEqual(string.Format(combinedTemplate, ";"), testCase.GetQueryString(separator: ";"));
             Assert.AreEqual(string.Format(splitTemplate, "&"), testCase.GetQueryString(splitMultiValues: true));
