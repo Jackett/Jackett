a449d6000415f49fee8b888bd99507e13d7a5bb1
Space <space.freespace@gmail.com>
2020-04-02 22:49:03 +0300

    #7004 - [FileList] Add API functionality

--- src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataFileList.cs
+++ src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataFileList.cs
@@ -1,15 +1,15 @@
 namespace Jackett.Common.Models.IndexerConfig.Bespoke
 {
-    internal class ConfigurationDataFileList : ConfigurationDataBasicLogin
+    internal class ConfigurationDataFileList : ConfigurationDataUserPasskey
     {
         public BoolItem IncludeRomanianReleases { get; private set; }
         public DisplayItem CatWarning { get; private set; }
 
         public ConfigurationDataFileList()
-            : base()
+            : base("Go into your filelist profile and copy the passkey.")
         {
-            IncludeRomanianReleases = new BoolItem() { Name = "IncludeRomanianReleases", Value = false };
-            CatWarning = new DisplayItem("When mapping TV ensure you add category 5000 in addition to 5030,5040.") { Name = "CatWarning" };
+            IncludeRomanianReleases = new BoolItem {Name = "IncludeRomanianReleases", Value = false};
+            CatWarning = new DisplayItem("When mapping TV ensure you add category 5000 in addition to 5030, 5040.") {Name = "CatWarning"};
         }
     }
 }
