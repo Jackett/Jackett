6afd992b9f81eef04997cb38b1025c872c7d72be
Garfield69 <garfield69@outlook.com>
2020-04-21 16:37:54 +1200

    filelist: update the info about passkey. 2caccea

--- src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataFileList.cs
+++ src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataFileList.cs
@@ -6,7 +6,7 @@ namespace Jackett.Common.Models.IndexerConfig.Bespoke
         public DisplayItem CatWarning { get; private set; }
 
         public ConfigurationDataFileList()
-            : base("Note this is <b>not</b> your <i>password</i>. Access your FileList account profile and copy the <b>passkey</b>.")
+            : base("Note this is <b>not</b> your <i>password</i>.<ul><li>Login to the FileList Website</li><li>Click on the <b>Profile</b> link</li><li>Scroll down to the <b>Reset Passkey</b> section</li><li>Copy the <b>passkey</b>.</li><li>Also be aware of not leaving a trailing blank at the end of the passkey after pasting it here.</li></ul>")
         {
             IncludeRomanianReleases = new BoolItem {Name = "IncludeRomanianReleases", Value = false};
             CatWarning = new DisplayItem("When mapping TV ensure you add category 5000 in addition to 5030, 5040.") {Name = "CatWarning"};
