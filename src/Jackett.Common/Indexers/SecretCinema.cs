507a6edc34b930a9c391eb530899016b6352dee6
Diego Heras <ngosang@hotmail.es>
2020-05-30 13:06:43 +0200

    secretcinema: remove ebook cat and add andmatch. resolves #8779 (#8812)

--- src/Jackett.Common/Indexers/SecretCinema.cs
+++ src/Jackett.Common/Indexers/SecretCinema.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
 using Jackett.Common.Indexers.Abstract;
 using Jackett.Common.Models;
 using Jackett.Common.Services.Interfaces;
@@ -33,35 +35,43 @@ namespace Jackett.Common.Indexers
 
             AddCategoryMapping(1, TorznabCatType.Movies, "Movies");
             AddCategoryMapping(2, TorznabCatType.Audio, "Music");
-            AddCategoryMapping(3, TorznabCatType.Books, "E-Books");
+            // cat=3 exists but it's required a refactor in Gazelle abstract to make it work
+            //AddCategoryMapping(3, TorznabCatType.Books, "E-Books");
+        }
+
+        protected override async Task<IEnumerable<ReleaseInfo>> PerformQuery(TorznabQuery query)
+        {
+            var results = await base.PerformQuery(query);
+            // results must contain search terms
+            results = results.Where(release => query.MatchQueryStringAND(release.Title));
+            return results;
         }
 
         protected override bool ReleaseInfoPostParse(ReleaseInfo release, JObject torrent, JObject result)
         {
             var media = (string)torrent["media"];
-            if (!string.IsNullOrEmpty(media))
+            if (string.IsNullOrEmpty(media))
+                return true;
+            switch (media)
             {
-                switch (media)
-                {
-                    case "SD":
-                        release.Category.Remove(TorznabCatType.Movies.ID);
-                        release.Category.Add(TorznabCatType.MoviesSD.ID);
-                        break;
-                    case "720p":
-                    case "1080p":
-                    case "4k": // not verified
-                        release.Category.Remove(TorznabCatType.Movies.ID);
-                        release.Category.Add(TorznabCatType.MoviesHD.ID);
-                        break;
-                    case "DVD-R":
-                        release.Category.Remove(TorznabCatType.Movies.ID);
-                        release.Category.Add(TorznabCatType.MoviesDVD.ID);
-                        break;
-                    case "BDMV":
-                        release.Category.Remove(TorznabCatType.Movies.ID);
-                        release.Category.Add(TorznabCatType.MoviesBluRay.ID);
-                        break;
-                }
+                case "SD":
+                    release.Category.Remove(TorznabCatType.Movies.ID);
+                    release.Category.Add(TorznabCatType.MoviesSD.ID);
+                    break;
+                case "720p":
+                case "1080p":
+                case "4k": // not verified
+                    release.Category.Remove(TorznabCatType.Movies.ID);
+                    release.Category.Add(TorznabCatType.MoviesHD.ID);
+                    break;
+                case "DVD-R":
+                    release.Category.Remove(TorznabCatType.Movies.ID);
+                    release.Category.Add(TorznabCatType.MoviesDVD.ID);
+                    break;
+                case "BDMV":
+                    release.Category.Remove(TorznabCatType.Movies.ID);
+                    release.Category.Add(TorznabCatType.MoviesBluRay.ID);
+                    break;
             }
             return true;
         }
