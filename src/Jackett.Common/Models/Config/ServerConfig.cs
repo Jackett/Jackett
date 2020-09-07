6b7583888a60c953e5a65d62198131eb5ec33550
ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>
2020-09-07 21:52:36 +0100

    proxy: disabled option

--- src/Jackett.Common/Models/Config/ServerConfig.cs
+++ src/Jackett.Common/Models/Config/ServerConfig.cs
@@ -69,8 +69,12 @@ namespace Jackett.Common.Models.Config
             {
                 url = $"{authString}@{url}";
             }
-
-            if (ProxyType != ProxyType.Http)
+            
+            if (ProxyType = ProxyType.Disabled)
+            {
+                var protocol = null;
+            }
+            else if (ProxyType != ProxyType.Http)
             {
                 var protocol = (Enum.GetName(typeof(ProxyType), ProxyType) ?? "").ToLower();
                 if (!string.IsNullOrEmpty(protocol))
