2cee966bf0d117c820d563f878330e8c410662d3
ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>
2020-09-07 23:36:35 +0100

    proxy: disabled option (edit2)

--- src/Jackett.Common/Utils/Clients/HttpWebClient2.cs
+++ src/Jackett.Common/Utils/Clients/HttpWebClient2.cs
@@ -65,7 +65,7 @@ namespace Jackett.Common.Utils.Clients
                 proxy.Dispose();
             webProxy = null;
             webProxyUrl = serverConfig.GetProxyUrl();
-            if (serverConfig.ProxyType = ProxyType.Disabled)
+            if (serverConfig.ProxyType == ProxyType.Disabled)
             {
                 webProxy = null;
             }
