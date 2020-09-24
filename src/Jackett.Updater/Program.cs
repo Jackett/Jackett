977279318d06692bfe87a172c7162ab3a406839a
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2020-09-23 22:13:17 -0400

    ThePirateBay: replace yml with c# and using TPB's API (#9593)

--- src/Jackett.Updater/Program.cs
+++ src/Jackett.Updater/Program.cs
@@ -393,6 +393,7 @@ namespace Jackett.Updater
                 "Definitions/theresurrection.yml",
                 "Definitions/thetorrents.yml",
                 "Definitions/the-madhouse.yml",
+                "Definitions/thepiratebay.yml", // migrated to c#
                 "Definitions/tigers-dl.yml",
                 "Definitions/tntvillage.yml",
                 "Definitions/torrentcouch.yml",
