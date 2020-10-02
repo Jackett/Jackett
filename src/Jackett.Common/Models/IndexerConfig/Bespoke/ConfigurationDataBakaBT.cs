b71805282e7b1bf1191e37a06431e4ffeac52b8d
Seeyabye <seeyabye91@gmail.com>
2020-10-02 10:51:19 +0900

    bakabt: fixed outstanding issues

---
+++ src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataBakaBT.cs
@@ -0,0 +1,15 @@
+namespace Jackett.Common.Models.IndexerConfig.Bespoke
+{
+    internal class ConfigurationDataBakaBT : ConfigurationDataBasicLogin
+    {
+        public BoolItem AddRomajiTitle { get; private set; }
+        public BoolItem AppendSeason { get; private set; }
+
+        public ConfigurationDataBakaBT(string instructionMessageOptional = null)
+            : base(instructionMessageOptional)
+        {
+            AddRomajiTitle = new BoolItem() { Name = "Add releases for Romaji Title", Value = true };
+            AppendSeason = new BoolItem() { Name = "Append Season for Sonarr Compatibility", Value = false };
+        }
+    }
+}
