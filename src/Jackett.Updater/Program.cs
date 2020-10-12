fed43f9f50ba1b79aae9e746ff071dca791da967
Ertugrul K <ertugrulk@users.noreply.github.com>
2020-10-12 22:51:29 +0200

    darmowetorrenty: migrate indexer to C# resolves #9782 (#9808)

--- src/Jackett.Updater/Program.cs
+++ src/Jackett.Updater/Program.cs
@@ -290,6 +290,7 @@ namespace Jackett.Updater
                 "Definitions/crazyscorner.yml",
                 "Definitions/czteam.yml",
                 "Definitions/cztorrent.yml",
+                "Definitions/darmowetorenty.yml", // migrated to C#
                 "Definitions/demonsite.yml",
                 "Definitions/digbt.yml",
                 "Definitions/downloadville.yml",
