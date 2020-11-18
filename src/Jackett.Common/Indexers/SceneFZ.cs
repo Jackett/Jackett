e4fd2741c3b7cc9c335971abda79335210fc1015
Garfield69 <garfield69@outlook.com>
2020-11-18 21:09:46 +1300

    scenefz: refresh cats

--- src/Jackett.Common/Indexers/SceneFZ.cs
+++ src/Jackett.Common/Indexers/SceneFZ.cs
@@ -47,53 +47,49 @@ namespace Jackett.Common.Indexers
                 logger: l,
                 p: ps)
         {
-            AddCategoryMapping(38, TorznabCatType.Movies, "Movies-Packs");
-            AddCategoryMapping(10, TorznabCatType.MoviesSD, "Movies-SD");
-            AddCategoryMapping(35, TorznabCatType.MoviesSD, "Movies-SD-RO");
-            AddCategoryMapping(8, TorznabCatType.MoviesHD, "Movies-HD");
-            AddCategoryMapping(29, TorznabCatType.MoviesHD, "Movies-HD-RO");
-            AddCategoryMapping(7, TorznabCatType.MoviesDVD, "Movies-DVD");
-            AddCategoryMapping(2, TorznabCatType.MoviesDVD, "Movies-DVD-RO");
-            AddCategoryMapping(17, TorznabCatType.MoviesBluRay, "Movies-BluRay");
-            AddCategoryMapping(24, TorznabCatType.MoviesBluRay, "Movies-BluRayRO");
-            AddCategoryMapping(59, TorznabCatType.MoviesForeign, "Movies-RO");
-            AddCategoryMapping(57, TorznabCatType.MoviesUHD, "Movies-UHD-RO");
-            AddCategoryMapping(61, TorznabCatType.MoviesUHD, "Movies-UHD");
-
-            AddCategoryMapping(41, TorznabCatType.TV, "TV-Packs");
-            AddCategoryMapping(66, TorznabCatType.TVForeign, "TV-Packs-RO");
-            AddCategoryMapping(45, TorznabCatType.TVSD, "TV-SD");
-            AddCategoryMapping(46, TorznabCatType.TVSD, "TV-SD-RO");
-            AddCategoryMapping(43, TorznabCatType.TVHD, "TV-HD");
-            AddCategoryMapping(44, TorznabCatType.TVHD, "TV-HD-RO");
-            AddCategoryMapping(60, TorznabCatType.TVForeign, "TV-RO");
-
-            AddCategoryMapping(11, TorznabCatType.PCGames, "Games-PC");
-            AddCategoryMapping(52, TorznabCatType.Console, "Games-Console");
-            AddCategoryMapping(1, TorznabCatType.PC0day, "Appz");
-            AddCategoryMapping(14, TorznabCatType.PC, "Linux");
-            AddCategoryMapping(37, TorznabCatType.PCMac, "Mac");
-            AddCategoryMapping(19, TorznabCatType.PCMobileOther, "Mobile");
-
-            AddCategoryMapping(62, TorznabCatType.TV, "Cartoons");
-            AddCategoryMapping(3, TorznabCatType.TVAnime, "Anime/Hentai");
-            AddCategoryMapping(6, TorznabCatType.Books, "eBooks");
+            AddCategoryMapping(38, TorznabCatType.Movies, "Movie Packs");
+            AddCategoryMapping(10, TorznabCatType.MoviesSD, "Movies: SD");
+            AddCategoryMapping(35, TorznabCatType.MoviesSD, "Movies: SD Ro");
+            AddCategoryMapping(8, TorznabCatType.MoviesHD, "Movies: HD");
+            AddCategoryMapping(29, TorznabCatType.MoviesHD, "Movies: HD Ro");
+            AddCategoryMapping(7, TorznabCatType.MoviesDVD, "Movies: DVD");
+            AddCategoryMapping(2, TorznabCatType.MoviesDVD, "Movies: DVD Ro");
+            AddCategoryMapping(17, TorznabCatType.MoviesBluRay, "Movies: BluRay");
+            AddCategoryMapping(24, TorznabCatType.MoviesBluRay, "Movies: BluRay Ro");
+            AddCategoryMapping(59, TorznabCatType.Movies, "Movies: Ro");
+            AddCategoryMapping(57, TorznabCatType.MoviesUHD, "Movies: 4K (2160p) Ro");
+            AddCategoryMapping(61, TorznabCatType.MoviesUHD, "Movies: 4K (2160p)");
+            AddCategoryMapping(41, TorznabCatType.TV, "TV Packs");
+            AddCategoryMapping(66, TorznabCatType.TV, "TV Packs Ro");
+            AddCategoryMapping(45, TorznabCatType.TVSD, "TV Episodes");
+            AddCategoryMapping(46, TorznabCatType.TVSD, "TV Episodes Ro");
+            AddCategoryMapping(43, TorznabCatType.TVHD, "TV Episodes HD");
+            AddCategoryMapping(44, TorznabCatType.TVHD, "TV Episodes HD Ro");
+            AddCategoryMapping(60, TorznabCatType.TV, "TV Ro");
+            AddCategoryMapping(11, TorznabCatType.PCGames, "Games: PC-ISO");
+            AddCategoryMapping(52, TorznabCatType.Console, "Games: Console");
+            AddCategoryMapping(1, TorznabCatType.PC0day, "Applications");
+            AddCategoryMapping(14, TorznabCatType.PC, "Applications: Linux");
+            AddCategoryMapping(37, TorznabCatType.PCMac, "Applications: Mac");
+            AddCategoryMapping(19, TorznabCatType.PCMobileOther, "Applications: Mobile");
+            AddCategoryMapping(62, TorznabCatType.TV, "TV Cartoons");
+            AddCategoryMapping(3, TorznabCatType.TVAnime, "TV Anime / Hentai");
+            AddCategoryMapping(6, TorznabCatType.BooksEBook, "E-books");
             AddCategoryMapping(5, TorznabCatType.Audio, "Music");
-            AddCategoryMapping(64, TorznabCatType.AudioVideo, "Music Videos");
+            AddCategoryMapping(64, TorznabCatType.AudioVideo, "Music Video");
             AddCategoryMapping(18, TorznabCatType.Other, "Images");
-            AddCategoryMapping(22, TorznabCatType.TVSport, "Sport");
-            AddCategoryMapping(58, TorznabCatType.TVSport, "Sports-RO");
-            AddCategoryMapping(9, TorznabCatType.TVDocumentary, "Documentary");
-            AddCategoryMapping(63, TorznabCatType.TVDocumentary, "Documentary-RO");
+            AddCategoryMapping(22, TorznabCatType.TVSport, "TV Sports");
+            AddCategoryMapping(58, TorznabCatType.TVSport, "TV Sports Ro");
+            AddCategoryMapping(9, TorznabCatType.TVDocumentary, "TV Documentary");
+            AddCategoryMapping(63, TorznabCatType.TVDocumentary, "TV Documentary Ro");
             AddCategoryMapping(65, TorznabCatType.Other, "Tutorial");
             AddCategoryMapping(67, TorznabCatType.OtherMisc, "Miscellaneous");
-
-            AddCategoryMapping(15, TorznabCatType.XXX, "XXX");
-            AddCategoryMapping(47, TorznabCatType.XXX, "XXX-DVD");
-            AddCategoryMapping(48, TorznabCatType.XXX, "XXX-HD");
-            AddCategoryMapping(49, TorznabCatType.XXXImageSet, "XXX-IMGSet");
-            AddCategoryMapping(50, TorznabCatType.XXX, "XXX-Packs");
-            AddCategoryMapping(51, TorznabCatType.XXX, "XXX-SD");
+            AddCategoryMapping(15, TorznabCatType.XXX, "XXX Movies");
+            AddCategoryMapping(47, TorznabCatType.XXX, "XXX DVD");
+            AddCategoryMapping(48, TorznabCatType.XXX, "XXX HD");
+            AddCategoryMapping(49, TorznabCatType.XXXImageSet, "XXX Images");
+            AddCategoryMapping(50, TorznabCatType.XXX, "XXX Packs");
+            AddCategoryMapping(51, TorznabCatType.XXX, "XXX SD");
         }
     }
 }
