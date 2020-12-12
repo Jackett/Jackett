690fa47ee8a7c271ef1ef59f2bbedff2628c106b
ngosang <ngosang@hotmail.es>
2020-12-12 18:59:44 +0100

    core: fix .net core 5.0 warnings. resolves #10433

--- src/Jackett.Common/Utils/EnvironmentUtil.cs
+++ src/Jackett.Common/Utils/EnvironmentUtil.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Reflection;
 
 namespace Jackett.Common.Utils
@@ -12,6 +13,16 @@ namespace Jackett.Common.Utils
             var assembly = Assembly.GetExecutingAssembly();
             var fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
             return $"v{fvi.ProductVersion}";
+        }
+
+        public static string JackettInstallationPath()
+        {
+            return Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location);
+        }
+
+        public static string JackettExecutablePath()
+        {
+            return Assembly.GetEntryAssembly()?.Location;
         }
 
         public static bool IsWindows => Environment.OSVersion.Platform == PlatformID.Win32NT;
