ed93a1cbebc3cb07ad0044348703f2b24b2c486d
Garfield69 <garfield69@outlook.com>
2021-09-18 06:57:27 +1200

    core: improve windows tray launcher code. #10068

--- jackett_launcher.bat
+++ jackett_launcher.bat
@@ -2,18 +2,17 @@
 :: https://github.com/Jackett/Jackett/issues/10068
 
 @echo off
+:: Allow the script to run (and work) from other directories
+cd /d "%~dp0"
 
 :: Wait until the updater ends
-:loop
-tasklist | find /i "JackettUpdater.exe" > nul 2>&1
-if errorlevel 1 (
-  goto continue
-) else (
-  echo JackettUpdater is still running
-  ping -n 2 127.0.0.1 > nul
-  goto loop
+:LOOP
+>nul 2>&1 tasklist | find /i "JackettUpdater.exe" || (
+    echo JackettUpdater is still running
+    >nul ping -n 2 127.0.0.1
+    goto :LOOP
 )
 
 :: Start Jackett Tray
-:continue
-start "" "%0\..\JackettTray.exe" --UpdatedVersion yes
+start "" "%~0\..\JackettTray.exe" --UpdatedVersion yes
+exit /b
