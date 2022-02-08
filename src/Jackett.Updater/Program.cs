4c8acbe9a4a1d57eef5c89679e243dffc0b3c90b
Garfield69 <garfield69@outlook.com>
2022-02-08 17:28:53 +1300

    lat-team: replaced by lat-team-api #8682

--- src/Jackett.Updater/Program.cs
+++ src/Jackett.Updater/Program.cs
@@ -381,6 +381,7 @@ namespace Jackett.Updater
                 "Definitions/kikibt.yml",
                 "Definitions/korsar.yml",
                 "Definitions/lapausetorrents.yml",
+                "Definitions/lat-team.yml", // switch to *-API #8682
                 "Definitions/latinop2p.yml",
                 "Definitions/leaguehd.yml", // renamed to lemonhd
                 "Definitions/lechaudron.yml",
