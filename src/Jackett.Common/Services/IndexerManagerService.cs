1447ade3f4ec9b45a20e7f74bf2b80d7c2c50c1e
ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>
2022-02-22 18:12:03 +0000

    Update IndexerManagerService.cs

--- src/Jackett.Common/Services/IndexerManagerService.cs
+++ src/Jackett.Common/Services/IndexerManagerService.cs
@@ -42,7 +42,6 @@ namespace Jackett.Common.Services
         {
             {"audiobooktorrents", "abtorrents"},
             {"broadcastthenet", "broadcasthenet"},
-            {"todotorrents", "dontorrent"},
             {"hdreactor", "hdhouse"},
             {"icetorrent", "speedapp"},
             {"kickasstorrent-kathow", "kickasstorrents-ws"},
@@ -54,12 +53,13 @@ namespace Jackett.Common.Services
             {"puntorrent", "puntotorrent"},
             {"rstorrent", "redstartorrent"},
             {"scenefz", "speedapp"},
+            {"seals", "greatposterwall"}
             {"tehconnectionme", "anthelion"},
+            {"todotorrents", "dontorrent"},
             {"torrentgalaxyorg", "torrentgalaxy"},
             {"transmithenet", "nebulance"},
             {"xtremezone", "speedapp"},
-            {"yourexotic", "exoticaz"},
-            {"seals", "greatposterwall"}
+            {"yourexotic", "exoticaz"}
         };
 
         public IndexerManagerService(IIndexerConfigurationService config, IProtectionService protectionService, WebClient webClient, Logger l, ICacheService cache, IProcessService processService, IConfigurationService globalConfigService, ServerConfig serverConfig)
