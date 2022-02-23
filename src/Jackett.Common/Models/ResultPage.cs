d4fe15b7b355b8213115d879734d68d4835525df
ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>
2022-02-23 01:00:24 +0000

    Update ResultPage.cs

--- src/Jackett.Common/Models/ResultPage.cs
+++ src/Jackett.Common/Models/ResultPage.cs
@@ -78,7 +78,7 @@ namespace Jackett.Common.Models
                             new XElement("title", RemoveInvalidXMLChars(r.Title)),
                             new XElement("guid", r.Guid.AbsoluteUri),  // GUID and (Link or Magnet) are mandatory
                             new XElement("jackettindexer", new XAttribute("id", r.Origin.Id), r.Origin.DisplayName),
-                            r.Details == null ? null : new XElement("comments", r.Details.AbsoluteUri),
+                            r.Details == null ? null : new XElement("details", r.Details.AbsoluteUri),
                             r.PublishDate == DateTime.MinValue ? new XElement("pubDate", XmlDateFormat(DateTime.Now)) : new XElement("pubDate", XmlDateFormat(r.PublishDate)),
                             r.Size == null ? null : new XElement("size", r.Size),
                             r.Files == null ? null : new XElement("files", r.Files),
