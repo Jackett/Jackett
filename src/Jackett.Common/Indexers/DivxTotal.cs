0d9ec1bfcb3102c7a6b9744067a087311c40568c
Garfield69 <garfield69@outlook.com>
2022-05-07 10:33:48 +1200

    divxtotal: new domain *.re -> *.ac

--- src/Jackett.Common/Indexers/DivxTotal.cs
+++ src/Jackett.Common/Indexers/DivxTotal.cs
@@ -57,6 +57,7 @@ namespace Jackett.Common.Indexers
             "https://www.divxtotal.nu/",
             "https://www.divxtotal.se/",
             "https://www.divxtotal.pm/",
+            "https://www.divxtotal.re/",
             "https://www.divxtotal.nl/"
         };
 
@@ -65,7 +66,7 @@ namespace Jackett.Common.Indexers
             : base(id: "divxtotal",
                    name: "DivxTotal",
                    description: "DivxTotal is a SPANISH site for Movies, TV series and Software",
-                   link: "https://www.divxtotal.re/",
+                   link: "https://www.divxtotal.ac/",
                    caps: new TorznabCapabilities
                    {
                        TvSearchParams = new List<TvSearchParam>
