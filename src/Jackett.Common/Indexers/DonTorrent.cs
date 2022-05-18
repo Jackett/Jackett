b1904738b0112a677adea4ee10a160d8e840c29e
Garfield69 <garfield69@outlook.com>
2022-05-19 09:02:28 +1200

    dontorrent: new domain *.bet -> *.cab

--- src/Jackett.Common/Indexers/DonTorrent.cs
+++ src/Jackett.Common/Indexers/DonTorrent.cs
@@ -39,7 +39,7 @@ namespace Jackett.Common.Indexers
         private const string SearchUrl = "buscar/";
 
         public override string[] AlternativeSiteLinks { get; protected set; } = {
-            "https://dontorrent.bet/",
+            "https://dontorrent.cab/",
             "https://todotorrents.net/",
             "https://tomadivx.net/",
             "https://seriesblanco.one/",
@@ -64,6 +64,7 @@ namespace Jackett.Common.Indexers
             "https://dontorrent.tel/",
             "https://dontorrent.nl/",
             "https://dontorrent.cx/",
+            "https://dontorrent.bet/",
             "https://dontorrent.eu/"
         };
 
@@ -82,7 +83,7 @@ namespace Jackett.Common.Indexers
             : base(id: "dontorrent",
                    name: "DonTorrent",
                    description: "DonTorrent is a SPANISH public tracker for MOVIES / TV / GENERAL",
-                   link: "https://dontorrent.bet/",
+                   link: "https://dontorrent.cab/",
                    caps: new TorznabCapabilities
                    {
                        TvSearchParams = new List<TvSearchParam>
