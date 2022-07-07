5f250e334726d0220331407791e633b2b080001c
Garfield69 <garfield69@outlook.com>
2022-07-08 07:22:52 +1200

    assorted: *.unblockit.name -> *.ist

--- src/Jackett.Common/Indexers/EpubLibre.cs
+++ src/Jackett.Common/Indexers/EpubLibre.cs
@@ -45,14 +45,11 @@ namespace Jackett.Common.Indexers
 
         public override string[] AlternativeSiteLinks { get; protected set; } = {
             "https://www.epublibre.org/",
-            "https://epublibre.unblockit.name/"
+            "https://epublibre.unblockit.ist/"
         };
 
         public override string[] LegacySiteLinks { get; protected set; } = {
-            "https://epublibre.unblockit.buzz/",
-            "https://epublibre.unblockit.club/",
             "https://epublibre.org/",
-            "https://epublibre.unblockit.onl/",
             "https://epublibre.unblockit.uno/",
             "https://epublibre.unblockit.ch/",
             "https://epublibre.unblockit.ws/",
@@ -65,7 +62,8 @@ namespace Jackett.Common.Indexers
             "https://epublibre.unblockit.biz/",
             "https://epublibre.unblockit.day/",
             "https://epublibre.unblockit.llc/",
-            "https://epublibre.unblockit.blue/"
+            "https://epublibre.unblockit.blue/",
+            "https://epublibre.unblockit.name/"
         };
 
         public EpubLibre(IIndexerConfigurationService configService, WebClient wc, Logger l, IProtectionService ps,
