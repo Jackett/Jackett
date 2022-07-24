9a724a3c411b7dde7c0652ac3dee67fc96b652b7
garfield69 <garfieldsixtynine@gmail.com>
2022-07-24 16:03:52 +1200

    Update TorznabQuery.cs

--- src/Jackett.Common/Models/TorznabQuery.cs
+++ src/Jackett.Common/Models/TorznabQuery.cs
@@ -33,7 +33,7 @@ namespace Jackett.Common.Models
         public string Label { get; set; }
         public string Track { get; set; }
         public int? Year { get; set; }
-        public ICollection<string> Genre { get; set; }
+        public string Genre { get; set; }
 
         public string Author { get; set; }
         public string Title { get; set; }
