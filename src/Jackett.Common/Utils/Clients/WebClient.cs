fa4b3e233b1d85cee485e2b074a836068ea939df
Garfield69 <garfield69@outlook.com>
2022-09-10 21:15:26 +1200

    Revert "webclient: also show response if the first char is a letter"
    This reverts commit 9a1aa101d7b5297b47d4c104fa4e3d44f2eb857d.

--- src/Jackett.Common/Utils/Clients/WebClient.cs
+++ src/Jackett.Common/Utils/Clients/WebClient.cs
@@ -195,7 +195,7 @@ namespace Jackett.Common.Utils.Clients
                 {
                     bodySize = result.ContentBytes.Length;
                     var contentString = result.ContentString.Trim();
-                    if (contentString.StartsWith("<") || contentString.StartsWith("{") || contentString.StartsWith("[") || Char.IsLetter(contentString[0]))
+                    if (contentString.StartsWith("<") || contentString.StartsWith("{") || contentString.StartsWith("["))
                         body = "\n" + contentString;
                     else
                         body = " <BINARY>";
