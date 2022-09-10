bb7865e6ef568ed8addca49e513bc5b797f98c46
ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>
2022-09-10 23:37:01 +0100

    Update ConfigurationDataAvistazTracker.cs

--- src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataAvistazTracker.cs
+++ src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataAvistazTracker.cs
@@ -8,6 +8,9 @@ namespace Jackett.Common.Models.IndexerConfig.Bespoke
         public BoolConfigurationItem Freeleech { get; private set; }
 
         public ConfigurationDataAvistazTracker()
-            => Freeleech = new BoolConfigurationItem("Search freeleech only") { Value = false };
+            : base("You have to check 'Enable RSS Feed' in 'My Account', without this configuration the torrent download does not work.<br/>You can find the PID in 'My profile'.")
+        {
+            Freeleech = new BoolConfigurationItem("Search freeleech only") { Value = false };
+        {
     }
 }
