56b0550ae3d0d9ac73c11d561cc02351d2781ddd
ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>
2022-09-10 23:39:54 +0100

    typo - beer was a poor choice...

--- src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataAvistazTracker.cs
+++ src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataAvistazTracker.cs
@@ -11,6 +11,6 @@ namespace Jackett.Common.Models.IndexerConfig.Bespoke
             : base("You have to check 'Enable RSS Feed' in 'My Account', without this configuration the torrent download does not work.<br/>You can find the PID in 'My profile'.")
         {
             Freeleech = new BoolConfigurationItem("Search freeleech only") { Value = false };
-        {
+        }
     }
 }
