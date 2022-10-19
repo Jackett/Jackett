88242ba9994793fba75d21b4da098f8edcccda3c
Garfield69 <garfield69@outlook.com>
2022-10-19 16:57:32 +1300

    rarbg: change applid #13510

--- src/Jackett.Common/Indexers/RarBG.cs
+++ src/Jackett.Common/Indexers/RarBG.cs
@@ -106,7 +106,7 @@ namespace Jackett.Common.Indexers
             AddCategoryMapping(53, TorznabCatType.ConsolePS4, "Games/PS4");
             AddCategoryMapping(54, TorznabCatType.MoviesHD, "Movies/x265/1080");
 
-            _appId = "jackett_" + EnvironmentUtil.JackettVersion();
+            _appId = "Jackett_" + EnvironmentUtil.JackettVersion();
 
             EnableConfigurableRetryAttempts();
         }
