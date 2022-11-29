94337b88bc92d7272e410d801711e7074e2daa9e
Garfield69 <garfield69@outlook.com>
2022-11-29 15:37:55 +1300

    dontorrent: new domain

--- src/Jackett.Common/Indexers/DonTorrent.cs
+++ src/Jackett.Common/Indexers/DonTorrent.cs
@@ -39,7 +39,7 @@ namespace Jackett.Common.Indexers
         private const string SearchUrl = "buscar/";
 
         public override string[] AlternativeSiteLinks { get; protected set; } = {
-            "https://dontorrent.click/",
+            "https://dontorrent.fail/",
             "https://todotorrents.net/",
             "https://tomadivx.net/",
             "https://seriesblanco.one/",
@@ -48,7 +48,6 @@ namespace Jackett.Common.Indexers
         };
 
         public override string[] LegacySiteLinks { get; protected set; } = {
-            "https://dontorrent.dev/",
             "https://dontorrent.bid/",
             "https://dontorrent.pet/",
             "https://dontorrent.soy/",
@@ -62,7 +61,8 @@ namespace Jackett.Common.Indexers
             "https://dontorrent.ltd/",
             "https://dontorrent.me/",
             "https://dontorrent.gs/",
-            "https://dontorrent.gy/"
+            "https://dontorrent.gy/",
+            "https://dontorrent.click/"
         };
 
         private static Dictionary<string, string> CategoriesMap => new Dictionary<string, string>
@@ -80,7 +80,7 @@ namespace Jackett.Common.Indexers
             : base(id: "dontorrent",
                    name: "DonTorrent",
                    description: "DonTorrent is a SPANISH public tracker for MOVIES / TV / GENERAL",
-                   link: "https://dontorrent.click/",
+                   link: "https://dontorrent.fail/",
                    caps: new TorznabCapabilities
                    {
                        TvSearchParams = new List<TvSearchParam>
