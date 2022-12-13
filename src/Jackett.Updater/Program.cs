b1595addb0ded4eb458d36e8bfff508d15a97695
ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>
2022-12-13 16:12:51 +0000

    Update Program.cs

--- src/Jackett.Updater/Program.cs
+++ src/Jackett.Updater/Program.cs
@@ -442,6 +442,7 @@ namespace Jackett.Updater
                 "Definitions/music-master.yml",
                 "Definitions/nachtwerk.yml",
                 "Definitions/nbtorrents.yml",
+                "Definitions/nbtorrents-api.yml",
                 "Definitions/netcosmo.yml",
                 "Definitions/netlab.yml",
                 "Definitions/newpct-me.yml",
