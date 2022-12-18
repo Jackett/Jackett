08206f4cfd79ad89f3ec189dd279172557e1048b
ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>
2022-12-18 05:44:48 +0000

    Update BroadcasTheNet.cs

--- src/Jackett.Common/Indexers/BroadcasTheNet.cs
+++ src/Jackett.Common/Indexers/BroadcasTheNet.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Jackett.Common.Models;
 using Jackett.Common.Models.IndexerConfig;
