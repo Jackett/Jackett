f882ef63a2fcc54df083a56e0010f7f8cbb4c79a
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-02-01 16:13:38 +0200

    speedcd: add freeleech only, exclude archives and fix TZ

---
+++ src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataSpeedCD.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Jackett.Common.Models.IndexerConfig.Bespoke
+{
+    [ExcludeFromCodeCoverage]
+    public class ConfigurationDataSpeedCD : ConfigurationDataBasicLogin
+    {
+        public BoolConfigurationItem Freeleech { get; set; }
+        public BoolConfigurationItem ExcludeArchives { get; set; }
+
+        public ConfigurationDataSpeedCD(string instructionMessageOptional = null)
+            : base(instructionMessageOptional)
+        {
+            Freeleech = new BoolConfigurationItem("Search freeleech only") { Value = false };
+            ExcludeArchives = new BoolConfigurationItem("Exclude torrents with RAR files") { Value = false };
+        }
+    }
+}
