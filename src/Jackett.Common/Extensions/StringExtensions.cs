421c55e0f10ad6ccfb001b74b39e8d8541bd62fb
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-03-05 00:25:12 +0200

    common: add more string extensions

--- src/Jackett.Common/Extensions/StringExtensions.cs
+++ src/Jackett.Common/Extensions/StringExtensions.cs
@@ -7,6 +7,18 @@ namespace Jackett.Common.Extensions
 {
     public static class StringExtensions
     {
+        public static string Join(this IEnumerable<string> values, string separator) => string.Join(separator, values);
+
+        public static bool IsNullOrWhiteSpace(this string text) => string.IsNullOrWhiteSpace(text);
+
+        public static bool IsNotNullOrWhiteSpace(this string text) => !string.IsNullOrWhiteSpace(text);
+
+        public static bool StartsWithIgnoreCase(this string text, string startsWith) => text.StartsWith(startsWith, StringComparison.InvariantCultureIgnoreCase);
+
+        public static bool EndsWithIgnoreCase(this string text, string startsWith) => text.EndsWith(startsWith, StringComparison.InvariantCultureIgnoreCase);
+
+        public static bool EqualsIgnoreCase(this string text, string equals) => text.Equals(equals, StringComparison.InvariantCultureIgnoreCase);
+
         public static bool ContainsIgnoreCase(this string source, string contains) => source != null && contains != null && CultureInfo.InvariantCulture.CompareInfo.IndexOf(source, contains, CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace) >= 0;
 
         public static bool ContainsIgnoreCase(this IEnumerable<string> source, string value) => source.Contains(value, StringComparer.InvariantCultureIgnoreCase);
