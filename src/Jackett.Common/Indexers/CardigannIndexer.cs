3b8aafa8a9af01cb09ba2de3700ae1876a60bc41
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-03-05 15:16:08 +0200

    add page size and `SupportsPagination`

--- src/Jackett.Common/Indexers/CardigannIndexer.cs
+++ src/Jackett.Common/Indexers/CardigannIndexer.cs
@@ -27,6 +27,8 @@ namespace Jackett.Common.Indexers
 {
     public class CardigannIndexer : BaseWebIndexer
     {
+        public override bool SupportsPagination => Definition.Search != null && Definition.Search.PageSize > 0;
+
         protected IndexerDefinition Definition;
         protected WebResult landingResult;
         protected IHtmlDocument landingResultDocument;
@@ -1310,8 +1312,6 @@ namespace Jackett.Common.Indexers
             variables[".Query.Year"] = query.Year?.ToString() ?? null;
             variables[".Query.Limit"] = query.Limit.ToString() ?? null;
             variables[".Query.Offset"] = query.Offset.ToString() ?? null;
-            variables[".Query.CurrentPage0"] = Math.Max(0, query.Offset > 0 && query.Limit > 0 ? query.Offset / query.Limit : 0).ToString();
-            variables[".Query.CurrentPage1"] = Math.Max(1, query.Offset > 0 && query.Limit > 0 ? (query.Offset / query.Limit) + 1 : 1).ToString();
             variables[".Query.Extended"] = query.Extended.ToString();
             variables[".Query.Categories"] = query.Categories;
             variables[".Query.APIKey"] = query.ApiKey;
@@ -1354,6 +1354,14 @@ namespace Jackett.Common.Indexers
                 KeywordTokens.Add((string)variables[".Query.Episode"]);
             variables[".Query.Keywords"] = string.Join(" ", KeywordTokens);
             variables[".Keywords"] = applyFilters((string)variables[".Query.Keywords"], Search.Keywordsfilters, variables);
+
+            var pageSize = Search.PageSize;
+
+            if (pageSize > 0)
+                variables[".PageSize"] = pageSize.ToString();
+
+            var page = query.Limit > 0 && query.Offset > 0 ? (query.Offset / query.Limit) + Search.FirstPageNumber : Search.FirstPageNumber;
+            variables[".Query.Page"] = page.ToString();
 
             // TODO: prepare queries first and then send them parallel
             var SearchPaths = Search.Paths;
