0910422976314c88207c65008f79e247395ebbe3
ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>
2023-04-03 09:59:21 +0100

    dariustracker: removed, down for 30 days. resolves #9768 (#14215)

--- src/Jackett.Updater/Program.cs
+++ src/Jackett.Updater/Program.cs
@@ -324,6 +324,7 @@ namespace Jackett.Updater
                 "Definitions/czteam.yml",
                 "Definitions/cztorrent.yml",
                 "Definitions/danishbytes.yml", // migrated to C#
+                "Definitions/dariustracker.yml",
                 "Definitions/dark-shadow.yml",
                 "Definitions/darktracker.yml",
                 "Definitions/darmowetorenty.yml", // migrated to C#
