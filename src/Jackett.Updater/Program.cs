ff6ea9c2eec7755dcfbeaaac49c2995a20570413
ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>
2023-08-18 01:16:20 +0100

    Update Program.cs

--- src/Jackett.Updater/Program.cs
+++ src/Jackett.Updater/Program.cs
@@ -559,6 +559,7 @@ namespace Jackett.Updater
                 "Definitions/soundpark.yml",
                 "Definitions/spacetorrent.yml",
                 "Definitions/speed-share.yml",
+                "Definitions/spidertk.yml",
                 "Definitions/sukebei-pantsu.yml",
                 "Definitions/swarmazon.yml", //  switched to *-API #13725
                 "Definitions/t411.yml",
