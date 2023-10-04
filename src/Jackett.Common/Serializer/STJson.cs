fdd2289920fa3f0f24b652686634b149eafc4cbb
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-10-04 05:14:21 +0300

    filelist: parse response with STJson

---
+++ src/Jackett.Common/Serializer/STJson.cs
@@ -0,0 +1,36 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Jackett.Common.Serializer
+{
+    public static class STJson
+    {
+        private static readonly JsonSerializerOptions _SerializerSettings = GetSerializerSettings();
+
+        public static JsonSerializerOptions GetSerializerSettings()
+        {
+            var settings = new JsonSerializerOptions();
+            ApplySerializerSettings(settings);
+            return settings;
+        }
+
+        public static void ApplySerializerSettings(JsonSerializerOptions serializerSettings)
+        {
+            serializerSettings.AllowTrailingCommas = true;
+            serializerSettings.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+            serializerSettings.PropertyNameCaseInsensitive = true;
+            serializerSettings.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+            serializerSettings.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            serializerSettings.WriteIndented = true;
+
+            serializerSettings.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase, true));
+            serializerSettings.Converters.Add(new BooleanConverter());
+        }
+
+        public static T Deserialize<T>(string json)
+            where T : new()
+        {
+            return JsonSerializer.Deserialize<T>(json, _SerializerSettings);
+        }
+    }
+}
