ae1193de9dbfd0a867179a10a2a9b290e92eee8c
ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>
2023-11-12 05:52:24 +0000

    cinecalidad: change domain wvw.*

--- src/Jackett.Common/Indexers/Cinecalidad.cs
+++ src/Jackett.Common/Indexers/Cinecalidad.cs
@@ -24,7 +24,7 @@ namespace Jackett.Common.Indexers
         public override string Id => "cinecalidad";
         public override string Name => "Cinecalidad";
         public override string Description => "Películas Full UHD/HD en Latino Dual.";
-        public override string SiteLink { get; protected set; } = "https://www.cinecalidad.so/";
+        public override string SiteLink { get; protected set; } = "https://wvw.cinecalidad.so/";
         public override string[] LegacySiteLinks => new[]
         {
             "https://www.cinecalidad.gs/",
@@ -43,6 +43,7 @@ namespace Jackett.Common.Indexers
             "https://vwv.cinecalidad.foo/",
             "https://wzw.cinecalidad.foo/",
             "https://v2.cinecalidad.foo/",
+            "https://www.cinecalidad.so/"
         };
         public override string Language => "es-419";
         public override string Type => "public";
