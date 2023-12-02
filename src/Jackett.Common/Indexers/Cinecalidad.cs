4963e3fd25b8a2e8223c0d7c3841465abe6b8a0e
Garfield69 <garfield69@outlook.com>
2023-12-02 17:09:02 +1300

    cinecalidad: new subdomain

--- src/Jackett.Common/Indexers/Cinecalidad.cs
+++ src/Jackett.Common/Indexers/Cinecalidad.cs
@@ -24,10 +24,9 @@ namespace Jackett.Common.Indexers
         public override string Id => "cinecalidad";
         public override string Name => "Cinecalidad";
         public override string Description => "Películas Full UHD/HD en Latino Dual.";
-        public override string SiteLink { get; protected set; } = "https://wv.cinecalidad.so/";
+        public override string SiteLink { get; protected set; } = "https://vvvv.cinecalidad.so/";
         public override string[] LegacySiteLinks => new[]
         {
-            "https://ww.cinecalidad.foo/",
             "https://v1.cinecalidad.foo/",
             "https://w.cinecalidad.foo/",
             "https://wwv.cinecalidad.foo/",
@@ -42,6 +41,7 @@ namespace Jackett.Common.Indexers
             "https://vvv.cinecalidad.so/",
             "https://ww.cinecalidad.so/",
             "https://w.cinecalidad.so/",
+            "https://wv.cinecalidad.so/",
         };
         public override string Language => "es-419";
         public override string Type => "public";
