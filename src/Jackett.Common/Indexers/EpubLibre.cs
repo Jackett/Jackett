e07e870a2fbd9157cb24ce4a3370622c6a81a9f1
Garfield69 <garfield69@outlook.com>
2024-01-08 06:15:05 +1300

    assorted: new unblockit domain

--- src/Jackett.Common/Indexers/EpubLibre.cs
+++ src/Jackett.Common/Indexers/EpubLibre.cs
@@ -26,12 +26,11 @@ namespace Jackett.Common.Indexers
         public override string[] AlternativeSiteLinks => new[]
         {
             "https://www.epublibre.org/",
-            "https://epublibre.unblockit.ing/"
+            "https://epublibre.unblockit.date/"
         };
         public override string[] LegacySiteLinks => new[]
         {
             "https://epublibre.org/",
-            "https://epublibre.unblockit.cat/",
             "https://epublibre.unblockit.nz/",
             "https://epublibre.unblockit.page/",
             "https://epublibre.unblockit.pet/",
@@ -46,6 +45,7 @@ namespace Jackett.Common.Indexers
             "https://epublibre.unblockit.esq/",
             "https://epublibre.unblockit.zip/",
             "https://epublibre.unblockit.foo/",
+            "https://epublibre.unblockit.ing/",
         };
         public override string Language => "es-ES";
         public override string Type => "public";
