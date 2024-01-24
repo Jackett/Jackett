f731f0a9fcdf0852f75d80bb85984d10ba908e93
swannie-eire <57441681+swannie-eire@users.noreply.github.com>
2024-01-24 19:47:09 +0000

    Update ApiSearch.cs
    adding IMDBid, TMDB, TVdb and TVmaze search to API json

--- src/Jackett.Common/Models/DTO/ApiSearch.cs
+++ src/Jackett.Common/Models/DTO/ApiSearch.cs
@@ -9,6 +9,10 @@ namespace Jackett.Common.Models.DTO
         public string Query { get; set; }
         public int[] Category { get; set; }
         public string[] Tracker { get; set; }
+        public string ImdbId { get; set; }
+        public int TvdbId { get; set; }
+        public int TmdbId { get; set; }
+        public int TvMazeId { get; set; }
 
         public static TorznabQuery ToTorznabQuery(ApiSearch request)
         {
@@ -50,6 +54,10 @@ namespace Jackett.Common.Models.DTO
 
             stringQuery.SearchTerm = queryStr;
             stringQuery.Categories = request.Category ?? Array.Empty<int>();
+            stringQuery.TmdbID = request.TmdbId;
+            stringQuery.TvdbID = request.TvdbId;
+            stringQuery.ImdbID = request.ImdbId;
+            stringQuery.TvmazeID = request.TvMazeId;
 
             // try to build an IMDB Query (tt plus 6 to 8 digits)
             if (stringQuery.SanitizedSearchTerm.StartsWith("tt") && stringQuery.SanitizedSearchTerm.Length <= 10)
@@ -66,6 +74,9 @@ namespace Jackett.Common.Models.DTO
                     };
                 }
             }
+
+
+
 
             return stringQuery;
         }
