59ec81b943ed4c8fd7e1ff93738c00584b905320
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-01-28 09:06:23 +0200

    core: update magnet trackers

--- src/Jackett.Common/Utils/MagnetUtil.cs
+++ src/Jackett.Common/Utils/MagnetUtil.cs
@@ -11,16 +11,16 @@ namespace Jackett.Common.Utils
         // Update best trackers from https://github.com/ngosang/trackerslist
         private static readonly NameValueCollection _Trackers = new NameValueCollection
         {
-            {"tr", "udp://tracker.opentrackr.org:1337/announce"},
+            {"tr", "http://tracker.opentrackr.org:1337/announce"},
+            {"tr", "udp://tracker.auctor.tv:6969/announce"},
             {"tr", "udp://opentracker.i2p.rocks:6969/announce"},
             {"tr", "https://opentracker.i2p.rocks:443/announce"},
-            {"tr", "http://tracker.openbittorrent.com:80/announce"},
-            {"tr", "udp://tracker.openbittorrent.com:6969/announce"},
             {"tr", "udp://open.demonii.com:1337/announce"},
+            {"tr", "udp://tracker.openbittorrent.com:6969/announce"},
+            {"tr", "http://tracker.openbittorrent.com:80/announce"},
             {"tr", "udp://open.stealth.si:80/announce"},
-            {"tr", "udp://exodus.desync.com:6969/announce"},
             {"tr", "udp://tracker.torrent.eu.org:451/announce"},
-            {"tr", "udp://tracker.moeking.me:6969/announce"},
+            {"tr", "udp://tracker.moeking.me:6969/announce"}
         };
 
         private static readonly string _TrackersEncoded = _Trackers.GetQueryString(null, true);
