dbac35253c8eaafe3e3838fe215c91036e7ea9da
Garfield69 <garfield69@outlook.com>
2024-01-30 19:38:44 +1300

    apisearch: additional to #15015
    add traktid, rageid, doubanid
    note that the GUI search has yet to be updated to support keyword-id=value parsing so that presumably will happen in the future.

--- src/Jackett.Common/Models/DTO/ApiSearch.cs
+++ src/Jackett.Common/Models/DTO/ApiSearch.cs
@@ -10,9 +10,12 @@ namespace Jackett.Common.Models.DTO
         public int[] Category { get; set; }
         public string[] Tracker { get; set; }
         public string ImdbId { get; set; }
+        public int? DoubanID { get; set; }
+        public int? RageID { get; set; }
         public int? TvdbId { get; set; }
         public int? TmdbId { get; set; }
         public int? TvMazeId { get; set; }
+        public int? TraktID { get; set; }
 
         public static TorznabQuery ToTorznabQuery(ApiSearch request)
         {
@@ -54,6 +57,9 @@ namespace Jackett.Common.Models.DTO
 
             stringQuery.SearchTerm = queryStr;
             stringQuery.Categories = request.Category ?? Array.Empty<int>();
+            stringQuery.DoubanID = request.DoubanID;
+            stringQuery.RageID = request.RageID;
+            stringQuery.TraktID = request.TraktID;
             stringQuery.TmdbID = request.TmdbId;
             stringQuery.TvdbID = request.TvdbId;
             stringQuery.ImdbID = request.ImdbId;
