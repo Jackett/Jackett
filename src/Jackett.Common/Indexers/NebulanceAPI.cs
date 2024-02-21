ec3d15d7402fe7eddaeac2cc335be07b61b06f39
vonvenera <vonvenera@gmail.com>
2024-02-22 00:02:18 +0100

    Move resolution search terms from name to tags in json

--- src/Jackett.Common/Indexers/NebulanceAPI.cs
+++ src/Jackett.Common/Indexers/NebulanceAPI.cs
@@ -34,6 +34,8 @@ namespace Jackett.Common.Indexers
         protected virtual string APIUrl => SiteLink + "api.php";
         protected virtual int KeyLength => 32;
 
+        private readonly string[] _moveToTags = { "720p", "1080p", "2160p", "4k" };
+
         // TODO: remove ConfigurationDataAPIKey class and use ConfigurationDataPasskey instead
         private new ConfigurationDataAPIKey configData
         {
@@ -170,7 +172,16 @@ namespace Jackett.Common.Indexers
 
             var searchString = query.GetQueryString();
             if (!string.IsNullOrWhiteSpace(searchString))
+            {
+                var searchTerms = Regex.Split(searchString, "\\s+").ToList();
+                var movingToTags = searchTerms.Intersect(_moveToTags, StringComparer.OrdinalIgnoreCase).ToList();
+                movingToTags.ForEach(
+                    tag => searchTerms.RemoveAll(searchTerm => searchTerm.Equals(tag, StringComparison.OrdinalIgnoreCase)));
+
+                searchString = searchTerms.Join(" ");
+                searchParam["tags"] = new JArray(movingToTags);
                 searchParam["name"] = "%" + Regex.Replace(searchString, "[\\W]+", "%").Trim() + "%";
+            }
 
             if (query.IsTvmazeQuery && query.TvmazeID.HasValue)
             {
