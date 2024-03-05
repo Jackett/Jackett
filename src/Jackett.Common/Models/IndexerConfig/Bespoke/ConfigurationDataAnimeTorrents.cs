51df69c5141cdc2ebc662fc15ae8ddebde187779
ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>
2024-03-05 20:10:13 +0000

    Update ConfigurationDataAnimeTorrents.cs

--- src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataAnimeTorrents.cs
+++ src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataAnimeTorrents.cs
@@ -7,11 +7,13 @@ namespace Jackett.Common.Models.IndexerConfig.Bespoke
     {
         public BoolConfigurationItem FreeleechOnly { get; private set; }
         public BoolConfigurationItem DownloadableOnly { get; private set; }
+        public DisplayInfoConfigurationItem AccountActivity { get; private set; }
 
         public ConfigurationDataAnimeTorrents()
         {
             FreeleechOnly = new BoolConfigurationItem("Show freeleech only") { Value = false };
             DownloadableOnly = new BoolConfigurationItem("Search downloadable torrents only (enable this only if your account class is Newbie)") { Value = false };
+            AccountActivity = new DisplayInfoConfigurationItem("Account Inactivity", "You must login and download at least 1 torrent per year or your account will be deleted. There is no re-activation. AnimeTorrents do not send email reminders, it is your responsibility.");
         }
     }
 }
