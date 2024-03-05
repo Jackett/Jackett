a92da63bcb54d656e060a3884cd2c613c4978850
ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>
2024-03-05 20:09:01 +0000

    Update ConfigurationDataAvistazTracker.cs

--- src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataAvistazTracker.cs
+++ src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataAvistazTracker.cs
@@ -6,11 +6,13 @@ namespace Jackett.Common.Models.IndexerConfig.Bespoke
     internal class ConfigurationDataAvistazTracker : ConfigurationDataBasicLoginWithPID
     {
         public BoolConfigurationItem Freeleech { get; private set; }
+        public DisplayInfoConfigurationItem AccountActivity { get; private set; }
 
         public ConfigurationDataAvistazTracker()
             : base("You have to check 'Enable RSS Feed' in 'My Account', without this configuration the torrent download does not work.<br/>You can find the PID in 'My profile'.")
         {
             Freeleech = new BoolConfigurationItem("Search freeleech only") { Value = false };
+            AccountActivity = new DisplayInfoConfigurationItem("Account Inactivity", "To avoid account deletion you must login at least 1 time every 90 days, and you must download at least 1 torrent every 6 months. Simply keeping torrents seeding long term will not protect your account. Do not rely on inactivity emails, admins often do not send them.");
         }
     }
 }
