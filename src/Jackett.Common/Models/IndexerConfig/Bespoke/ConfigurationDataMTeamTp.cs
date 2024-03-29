2ae0a35a3478329bc7bd173c95eb10004c18a051
Garfield69 <garfield69@outlook.com>
2024-03-29 15:56:58 +1300

    m-teamtp: add config info for apikey. #15209

--- src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataMTeamTp.cs
+++ src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataMTeamTp.cs
@@ -6,11 +6,13 @@ namespace Jackett.Common.Models.IndexerConfig.Bespoke
     internal class ConfigurationDataMTeamTp : ConfigurationData
     {
         public StringConfigurationItem ApiKey { get; private set; }
+        public DisplayInfoConfigurationItem ApiKeyInfo { get; private set; }
         public BoolConfigurationItem FreeleechOnly { get; private set; }
 
         public ConfigurationDataMTeamTp()
         {
             ApiKey = new StringConfigurationItem("API Key");
+            ApiKeyInfo = new DisplayInfoConfigurationItem("ApiKey Info", "The API key can be obtained by accessing your M-Team TP user CP.");
             FreeleechOnly = new BoolConfigurationItem("Search freeleech only") { Value = false };
         }
     }
