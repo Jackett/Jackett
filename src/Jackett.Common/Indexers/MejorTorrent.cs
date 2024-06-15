184b4033d7052fa9946b977063e29f11c918ef86
Garfield69 <garfield69@outlook.com>
2024-06-16 05:15:51 +1200

    mejortorrent: bump domain

--- src/Jackett.Common/Indexers/MejorTorrent.cs
+++ src/Jackett.Common/Indexers/MejorTorrent.cs
@@ -27,12 +27,11 @@ namespace Jackett.Common.Indexers
         public override string Id => "mejortorrent";
         public override string Name => "MejorTorrent";
         public override string Description => "MejorTorrent - Hay veces que un torrent viene mejor! :)";
-        public override string SiteLink { get; protected set; } = "https://www17.mejortorrent.zip/";
+        public override string SiteLink { get; protected set; } = "https://www18.mejortorrent.zip/";
         public override string[] LegacySiteLinks => new[]
         {
             "https://mejortorrent.unblockit.click/",
             "https://mejortorrent.unblockit.asia/",
-            "https://www2.mejortorrent.rip/",
             "https://mejortorrent.unblockit.mov/",
             "https://www3.mejortorrent.rip/",
             "https://www4.mejortorrent.rip/",
@@ -53,6 +52,7 @@ namespace Jackett.Common.Indexers
             "https://www14.mejortorrent.rip/",
             "https://www15.mejortorrent.rip/",
             "https://www16.mejortorrent.rip/",
+            "https://www17.mejortorrent.zip/",
         };
         public override string Language => "es-ES";
         public override string Type => "public";
