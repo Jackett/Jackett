f4f06c8f8e606b6be47b41b2a00e82244eaabb08
ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>
2024-06-19 06:42:20 +0100

    homeporntorrents: restore indexer. resolves #14522 (#15428)

--- src/Jackett.Updater/Program.cs
+++ src/Jackett.Updater/Program.cs
@@ -444,7 +444,6 @@ namespace Jackett.Updater
                 "Definitions/heavens-hell.yml",
                 "Definitions/hellastz.yml",
                 "Definitions/hidden-palace.yml",
-                "Definitions/homeporntorrents.yml",
                 "Definitions/hon3yhd-net.yml",
                 "Definitions/hon3yhd.yml",
                 "Definitions/horriblesubs.yml",
