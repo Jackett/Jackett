9d6a119012ac5c5443fd17dc87b9b1bda4691727
Garfield69 <garfield69@outlook.com>
2024-08-02 05:06:17 +1200

    dontorrent: new domain

--- src/Jackett.Common/Indexers/Definitions/DonTorrent.cs
+++ src/Jackett.Common/Indexers/Definitions/DonTorrent.cs
@@ -28,10 +28,10 @@ namespace Jackett.Common.Indexers.Definitions
         public override string Name => "DonTorrent";
         public override string Description => "DonTorrent is a SPANISH public tracker for MOVIES / TV / GENERAL";
         // in the event the redirect is inactive https://t.me/s/dontorrent should have the latest working domain
-        public override string SiteLink { get; protected set; } = "https://dontorrent.cricket/";
+        public override string SiteLink { get; protected set; } = "https://dontorrent.earth/";
         public override string[] AlternativeSiteLinks => new[]
         {
-            "https://dontorrent.cricket/",
+            "https://dontorrent.earth/",
             "https://todotorrents.org/",
             "https://tomadivx.net/",
             "https://seriesblanco.one/",
@@ -40,7 +40,6 @@ namespace Jackett.Common.Indexers.Definitions
         };
         public override string[] LegacySiteLinks => new[]
         {
-            "https://dontorrent.agency/",
             "https://dontorrent.skin/",
             "https://dontorrent.directory/",
             "https://dontorrent.boutique/",
@@ -55,6 +54,7 @@ namespace Jackett.Common.Indexers.Definitions
             "https://dontorrent.city/",
             "https://dontorrent.cologne/",
             "https://dontorrent.dance/",
+            "https://dontorrent.cricket/",
         };
         public override string Language => "es-ES";
         public override string Type => "public";
