caf86d0927b00284874ddbf85efefbf16818c126
Victor Guerra Veloso <victorgvbh@gmail.com>
2024-11-19 12:15:37 -0500

    Apply size validation consistently in missing scenarios

--- src/Jackett.Common/Indexers/Definitions/RedeTorrent.cs
+++ src/Jackett.Common/Indexers/Definitions/RedeTorrent.cs
@@ -142,7 +142,8 @@ namespace Jackett.Common.Indexers.Definitions
                     release.Languages = fileInfo.Audio?.ToList() ?? release.Languages;
                     release.Genres = fileInfo.Genres?.ToList() ?? release.Genres;
                     release.Subs = string.IsNullOrEmpty(fileInfo.Subtitle) ? release.Subs : new[] { fileInfo.Subtitle };
-                    release.Size = !string.IsNullOrEmpty(fileInfo.Size) ? ParseUtil.GetBytes(fileInfo.Size) : ExtractSizeByResolution(release.Title);
+                    var size = ParseUtil.GetBytes(fileInfo.Size ?? string.Empty);
+                    release.Size = size > 0 ? size : ExtractSizeByResolution(release.Title);
 
                     if (release.Title.IsNotNullOrWhiteSpace())
                         releases.Add(release);
