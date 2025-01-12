620925de82560fbbd824c4bd239d8a4ab0cab5eb
Garfield69 <garfield69@outlook.com>
2025-01-13 06:15:53 +1300

    fix cfce8f987f076968826f1ace672b8a63ba52aaaf

--- src/Jackett.Common/Extensions/IEnumerableExtensions.cs
+++ src/Jackett.Common/Extensions/IEnumerableExtensions.cs
@@ -4,6 +4,6 @@ namespace Jackett.Common.Extensions
 {
     public static class IEnumerableExtensions
     {
-        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source, IEqualityComparer<T> comparer = null) => new (source, comparer);
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source, IEqualityComparer<T> comparer = null) => new(source, comparer);
     }
 }
