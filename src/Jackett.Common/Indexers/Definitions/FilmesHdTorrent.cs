aed15079ceebef523ea603858310157d1b72fdfb
Garfield69 <garfield69@outlook.com>
2025-04-01 20:21:04 +1300

    filmeshdtorrent: drop alternate domain. #15718

--- src/Jackett.Common/Indexers/Definitions/FilmesHdTorrent.cs
+++ src/Jackett.Common/Indexers/Definitions/FilmesHdTorrent.cs
@@ -23,7 +23,6 @@ namespace Jackett.Common.Indexers.Definitions
 
         public override string[] AlternativeSiteLinks { get; protected set; } = {
             "https://www.filmeshdtorrent.vip/",
-            "https://bludvfilmes.org/rarbg/",
         };
 
         public override string[] LegacySiteLinks { get; protected set; } = {
@@ -33,6 +32,7 @@ namespace Jackett.Common.Indexers.Definitions
             "https://filmetorrent.org/baixar/",
             "https://filmetorrent.org/x/",
             "https://bludvfilmes.org/",
+            "https://bludvfilmes.org/rarbg/",
         };
 
         public FilmesHdTorrent(IIndexerConfigurationService configService, WebClient wc, Logger l, IProtectionService ps, ICacheService cs) : base(configService, wc, l, ps, cs)
