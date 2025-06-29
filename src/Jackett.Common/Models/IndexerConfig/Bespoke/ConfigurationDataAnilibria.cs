0c31823669977e96b1c78ec262662507c495b3d6
impr3ssi0n <mbondarev@levelgroup.ru>
2025-06-29 21:33:44 +0300

    anilibria: fix styles.

--- src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataAnilibria.cs
+++ src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataAnilibria.cs
@@ -7,7 +7,7 @@ namespace Jackett.Common.Models.IndexerConfig.Bespoke
         public ConfigurationDataAnilibria()
         {
             AddRussianToTitle = new BoolConfigurationItem("Add RUS to end of all titles to improve language detection by Sonarr and Radarr. Will cause English-only results to be misidentified.") { Value = false };
-            AddSeasonToTitle = new BoolConfigurationItem("Try to add the season number to the title.") {Value = false};
+            AddSeasonToTitle = new BoolConfigurationItem("Try to add the season number to the title.") { Value = false };
         }
     }
 }
