44f6755532c8417e666c6aa3a836bdb8023dcf23
Garfield69 <garfield69@outlook.com>
2025-08-12 08:57:29 +1200

    filelist:: update config

--- src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataFileList.cs
+++ src/Jackett.Common/Models/IndexerConfig/Bespoke/ConfigurationDataFileList.cs
@@ -5,16 +5,14 @@ namespace Jackett.Common.Models.IndexerConfig.Bespoke
     [ExcludeFromCodeCoverage]
     internal class ConfigurationDataFileList : ConfigurationDataUserPasskey
     {
-        public DisplayInfoConfigurationItem WhiteList { get; set; }
         public BoolConfigurationItem Freeleech { get; set; }
         public DisplayInfoConfigurationItem CatWarning { get; set; }
 
         public ConfigurationDataFileList()
             : base("Note this is <b>not</b> your <i>password</i>.<ul><li>Login to the FileList Website</li><li>Click on the <b>Profile</b> link</li><li>Scroll down to the <b>Reset Passkey</b> section</li><li>Copy the <b>passkey</b>.</li><li>Also be aware of not leaving a trailing blank at the end of the passkey after pasting it here.</li></ul>")
         {
-            WhiteList = new DisplayInfoConfigurationItem("WhiteList", "Before using this indexer you need to fill the Whitelist IP for API on Filelist profile settings page for it to work.");
             Freeleech = new BoolConfigurationItem("Search freeleech only") { Value = false };
-            CatWarning = new DisplayInfoConfigurationItem("CatWarning", "When mapping TV ensure you add category 5000 in addition to 5030, 5040.");
+            CatWarning = new DisplayInfoConfigurationItem("CatWarning", "When mapping TV ensure you add category 5000 in addition to 5020, 5030, 5040, 5045.");
         }
     }
 }
