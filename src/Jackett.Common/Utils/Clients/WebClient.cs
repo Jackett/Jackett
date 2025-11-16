61b263dd98a989c5cbd28c10fd1a413a7c5d0a1a
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-11-16 22:05:22 +0200

    core: refactor obsolete calls (#16290)
    * core: refactor obsolete hashing calls
    
    * core: delete cert callback validation removal in update service
    
    * core: remove deprecated ServicePointManager calls
    
    - Avoid harcoding SSL protocols.
    - Lowering DefaultConnectionLimit 1000 to MaxConnectionsPerServer 20
    
    * core: avoid is only supported on windows warnings

--- src/Jackett.Common/Utils/Clients/WebClient.cs
+++ src/Jackett.Common/Utils/Clients/WebClient.cs
@@ -220,11 +220,7 @@ namespace Jackett.Common.Utils.Clients
             return result;
         }
 
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
-        protected virtual async Task<WebResult> Run(WebRequest webRequest) => throw new NotImplementedException();
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
-
-        public virtual void Init() => ServicePointManager.DefaultConnectionLimit = 1000;
+        protected virtual Task<WebResult> Run(WebRequest webRequest) => throw new NotImplementedException();
 
         public virtual void OnCompleted() => throw new NotImplementedException();
 
