3ab127a4159032833ae6e7f22f841ff6687583a7
KaiHuaDou <74781268+KaiHuaDou@users.noreply.github.com>
2026-01-02 22:57:32 +0800

    Refresh whole WebUI

---
+++ src/Jackett.Common/Content/dist/assets/login-legacy.js
@@ -0,0 +1,1 @@
+System.register(["./main-legacy.js"],function(e,t){"use strict";return{setters:[null],execute:function(){}}});
---
+++ src/Jackett.Common/Content/dist/assets/login.js
@@ -0,0 +1,1 @@
+/* empty css    */function t(){import.meta.url,import("_").catch(()=>1),(async function*(){})().next()}export{t as __vite_legacy_guard};
