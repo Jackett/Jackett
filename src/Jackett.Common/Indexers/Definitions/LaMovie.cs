dcf2f21e297fa8f82008dd2e5e829351f0c0ef7c
Memo Cachú <gcachu.o@gmail.com>
2026-01-24 00:12:30 -0600

    setup login

--- src/Jackett.Common/Indexers/Definitions/LaMovie.cs
+++ src/Jackett.Common/Indexers/Definitions/LaMovie.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
+using AngleSharp.Dom;
+using AngleSharp.Html.Parser;
 using Jackett.Common.Models;
+using Jackett.Common.Models.IndexerConfig;
 using Jackett.Common.Models.IndexerConfig.Bespoke;
 using Jackett.Common.Services.Interfaces;
 using Jackett.Common.Utils.Clients;
@@ -21,6 +24,13 @@ namespace Jackett.Common.Indexers.Definitions
         public override string SiteLink { get; protected set; } = "https://la.movie/";
         public override string Language => "es-419";
         public override string Type => "semi-private";
+        private string LoginUrl => SiteLink + "wp-json/wpf/v1/auth/login";
+
+        private ConfigurationDataLaMovie Configuration
+        {
+            get => (ConfigurationDataLaMovie)configData;
+            set => configData = value;
+        }
 
         public override TorznabCapabilities TorznabCaps => SetCapabilities();
 
@@ -55,17 +65,65 @@ namespace Jackett.Common.Indexers.Definitions
         {
             LoadValuesFromJson(configJson);
 
-            var releases = await PerformQuery(new());
+            var payload = new JObject
+            {
+                ["email"] = Configuration.Email.Value,
+                ["password"] = Configuration.Password.Value
+            }.ToString();
 
-            await ConfigureIfOK(string.Empty, releases.Any(), () =>
-                                    throw new("Could not find release from this URL."));
+            var headers = new Dictionary<string, string>
+            {
+                ["Content-Type"] = "application/json",
+                ["Accept"] = "application/json"
+            };
+
+            var result = await RequestWithCookiesAndRetryAsync(
+                LoginUrl,
+                cookieOverride: CookieHeader,
+                method: RequestType.POST,
+                referer: SiteLink,
+                data: null,
+                headers: headers,
+                rawbody: payload
+                );
+
+            var json = JObject.Parse(result.ContentString);
+            var token = json.SelectToken("data.token")?.ToString();
+
+            await ConfigureIfOK(token, IsAuthorized(token), () =>
+            {
+                var contentString = result.ContentString;
+                var json = JObject.Parse(contentString);
+                var errorMessage = json.Value<string>("msg");
+                throw new ExceptionWithConfigData(errorMessage, Configuration);
+            });
 
             return IndexerConfigurationStatus.Completed;
+        }
+
+        private bool IsAuthorized(string token) => string.IsNullOrWhiteSpace(token)
+            ? throw new ExceptionWithConfigData("Login response did not contain a token", Configuration) : true;
+
+        protected new async Task ConfigureIfOK(string token, bool isLoggedin, Func<Task> onError)
+        {
+            if (isLoggedin)
+            {
+                configData.AddDynamic("authToken", new ConfigurationData.StringConfigurationItem("Auth Token") { Value = token });
+                IsConfigured = true;
+                SaveConfig();
+            }
+            else
+            {
+                await onError();
+            }
         }
 
         protected override async Task<IEnumerable<ReleaseInfo>> PerformQuery(TorznabQuery query)
         {
             var releases = new List<ReleaseInfo>();
+
+            var searchTerm = "";
+            var searchUrl = $"/wp-api/v1/search?filter=%7B%7D&postType=any&q={searchTerm}&postsPerPage=26";
 
             return releases;
         }
