4b04f5986c6a6830bc8203f54be42719a46e6d87
VuzzyM <>
2026-03-18 17:33:19 +0200

    Fixes invalid BEncodedDictionary

--- src/Jackett.Server/Controllers/DownloadController.cs
+++ src/Jackett.Server/Controllers/DownloadController.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BencodeNET.Objects;
@@ -42,7 +44,6 @@ namespace Jackett.Server.Controllers
                     return Unauthorized();
 
                 var indexer = _indexerService.GetWebIndexer(indexerId);
-
                 if (!indexer.IsConfigured)
                 {
                     _logger.Warn($"Rejected a request to {indexer.Name} which is unconfigured.");
@@ -71,18 +72,79 @@ namespace Jackett.Server.Controllers
                 }
 
                 // This will fix torrents where the keys are not sorted, and thereby not supported by Sonarr.
+                // Fix torrents with unsorted top-level keys
                 byte[] sortedDownloadBytes;
                 try
                 {
-                    var parser = new BencodeParser();
-                    var torrentDictionary = parser.Parse(downloadBytes);
-                    sortedDownloadBytes = torrentDictionary.EncodeAsBytes();
+                    if (downloadBytes.Length > 0 && downloadBytes[0] == (byte)'d')
+                    {
+                        int i = 1;
+                        var items = new List<(byte[] key, byte[] value)>();
+                        while (i < downloadBytes.Length && downloadBytes[i] != (byte)'e')
+                        {
+                            // Read key
+                            int colon = Array.IndexOf(downloadBytes, (byte)':', i);
+                            if (colon == -1) break;
+                            int keyLen = int.Parse(Encoding.ASCII.GetString(downloadBytes, i, colon - i));
+                            int keyStart = colon + 1;
+                            int keyEnd = keyStart + keyLen;
+                            if (keyEnd > downloadBytes.Length) break;
+                            var key = new byte[keyEnd - keyStart];
+                            Array.Copy(downloadBytes, keyStart, key, 0, keyEnd - keyStart);
+                            i = keyEnd;
+                            // Read value
+                            int valStart = i;
+                            i = SkipBencodeElement(downloadBytes, i);
+                            var val = new byte[i - valStart];
+                            Array.Copy(downloadBytes, valStart, val, 0, i - valStart);
+                            items.Add((key, val));
+                        }
+                        // Rebuild sorted top-level dictionary
+                        var list = new List<byte> { (byte)'d' };
+                        foreach (var kv in items.OrderBy(it => Encoding.ASCII.GetString(it.key)))
+                        {
+                            var keyBytes = Encoding.ASCII.GetBytes(kv.key.Length + ":");
+                            list.AddRange(keyBytes);
+                            list.AddRange(kv.key);
+                            list.AddRange(kv.value);
+                        }
+                        list.Add((byte)'e');
+                        sortedDownloadBytes = list.ToArray();
+                    }
+                    else
+                    {
+                        var parser = new BencodeParser();
+                        var torrentDictionary = parser.Parse(downloadBytes);
+                        sortedDownloadBytes = torrentDictionary.EncodeAsBytes();
+                    }
                 }
                 catch (Exception e)
                 {
                     var content = indexer.Encoding.GetString(downloadBytes);
                     _logger.Error(content);
                     throw new Exception("BencodeParser failed", e);
+                }
+
+                int SkipBencodeElement(byte[] data, int index)
+                {
+                    if (index >= data.Length) return index;
+                    var c = data[index];
+                    if (c == (byte)'i')
+                        return Array.IndexOf(data, (byte)'e', index) + 1;
+                    if (c == (byte)'l' || c == (byte)'d')
+                    {
+                        index++;
+                        while (index < data.Length && data[index] != (byte)'e')
+                            index = SkipBencodeElement(data, index);
+                        return index + 1;
+                    }
+                    if (c >= (byte)'0' && c <= (byte)'9')
+                    {
+                        int colon = Array.IndexOf(data, (byte)':', index);
+                        int len = int.Parse(Encoding.ASCII.GetString(data, index, colon - index));
+                        return colon + 1 + len;
+                    }
+                    return index + 1;
                 }
 
                 var fileName = StringUtil.MakeValidFileName(file, '_', false) + ".torrent"; // call MakeValidFileName again to avoid any kind of injection attack
