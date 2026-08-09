be1f04dc66c0c2e934059f84ecb8e0c1729d7d19
ambitionmajor <315087608+ambitionmajor@users.noreply.github.com>
2026-08-09 23:39:53 +0200

    norbits: avoid login preflight request (#17001)
    * Retry NorBits searches after session expiry
    
    * Apply suggestions from code review
    
    Co-authored-by: Bogdan <mynameisbogdan@users.noreply.github.com>
    
    * Normalize NorBits line endings
    
    ---------
    
    Co-authored-by: Bogdan <mynameisbogdan@users.noreply.github.com>

--- src/Jackett.Common/Indexers/Definitions/NorBits.cs
+++ src/Jackett.Common/Indexers/Definitions/NorBits.cs
@@ -199,29 +199,6 @@ namespace Jackett.Common.Indexers.Definitions
         }
 
         /// <summary>
-        /// Check logged-in state for provider
-        /// </summary>
-        /// <returns></returns>
-        private async Task CheckLoginAsync()
-        {
-            // Checking ...
-            logger.Debug("NorBits -  Checking logged-in state....");
-            var loggedInCheck = await RequestWithCookiesAsync(SearchUrl);
-            if (!loggedInCheck.ContentString.Contains("logout.php"))
-            {
-                // Cookie expired, renew session on provider
-                logger.Debug("NorBits - Not logged, login now...\n");
-
-                await DoLoginAsync();
-            }
-            else
-            {
-                // Already logged, session active
-                logger.Debug("NorBits - Already logged, continue...\n");
-            }
-        }
-
-        /// <summary>
         /// Execute our search query
         /// </summary>
         /// <param name="query">Query</param>
@@ -231,9 +208,6 @@ namespace Jackett.Common.Indexers.Definitions
             var releases = new List<ReleaseInfo>();
             var exactSearchTerm = query.GetQueryString();
             var searchUrl = SearchUrl;
-
-            // Check login before performing a query
-            await CheckLoginAsync();
 
             var searchTerms = new List<string> { exactSearchTerm };
 
@@ -250,9 +224,10 @@ namespace Jackett.Common.Indexers.Definitions
                 var request = BuildQuery(searchTerm, query, searchUrl);
 
                 // Getting results & Store content
-                var response = await RequestWithCookiesAndRetryAsync(request, ConfigData.CookieHeader.Value);
+                var response = await SearchWithLoginRetryAsync(request);
+
                 var parser = new HtmlParser();
-                using var dom = parser.ParseDocument(response.ContentString);
+                using var dom = await parser.ParseDocumentAsync(response.ContentString);
 
                 try
                 {
@@ -350,6 +325,29 @@ namespace Jackett.Common.Indexers.Definitions
             }
             // Return found releases
             return releases;
+        }
+
+        private async Task<WebResult> SearchWithLoginRetryAsync(string request)
+        {
+            var response = await RequestWithCookiesAndRetryAsync(request, ConfigData.CookieHeader.Value);
+
+            if (response.ContentString.Contains("logout.php"))
+            {
+                return response;
+            }
+
+            logger.Debug("NorBits: Invalid session, logging in and retrying search");
+
+            await DoLoginAsync();
+
+            response = await RequestWithCookiesAndRetryAsync(request, ConfigData.CookieHeader.Value);
+
+            if (!response.ContentString.Contains("logout.php"))
+            {
+                throw new Exception("NorBits login succeeded, but the retried search is not authenticated.");
+            }
+
+            return response;
         }
 
         /// <summary>
