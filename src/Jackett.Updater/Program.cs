53714387fe24f19487dc4364ae2e278346e4b329
Garfield69 <garfield69@outlook.com>
2026-08-13 06:15:53 +1200

    720pier: removed. resolves #632

--- src/Jackett.Updater/Program.cs
+++ src/Jackett.Updater/Program.cs
@@ -654,6 +654,7 @@ namespace Jackett.Updater
                 "Definitions/partis.yml",
                 "Definitions/passionetorrent.yml",
                 "Definitions/peerjunkies.yml",
+                "Definitions/pier720.yml",
                 "Definitions/piratadigital.yml",
                 "Definitions/piratbit.yml",
                 "Definitions/piratbitl.yml",
